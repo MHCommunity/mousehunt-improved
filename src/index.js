86eaba305e129afa2abae10a1f06082f25ad91e7
Brad Parbs <brad@bradparbs.com>
2023-04-24 16:23:17 -0500

    update module desc

--- src/index.js
+++ src/index.js
@@ -45,12 +45,12 @@ const modules = [
     name: 'Core Modules',
     description: 'Interface and functionality improvements',
     modules: [
-      { id: 'better-mh-better-ui', name: 'Better UI', default: true, description: '', load: betterUi },
-      { id: 'better-mh-better-friends', name: 'Better Friends', default: true, description: '', load: betterFriends },
-      { id: 'better-mh-better-inventory', name: 'Better Inventory', default: true, description: '', load: betterInventory },
+      { id: 'better-mh-better-ui', name: 'Better UI', default: true, description: 'Updates the MH interface with a variety of UI and style changes.', load: betterUi },
+      { id: 'better-mh-better-friends', name: 'Better Friends', default: true, description: 'Hover on a name anywhere and get a quick popup of the friend interface. View details, send supplies in a click, etc.', load: betterFriends },
+      { id: 'better-mh-better-inventory', name: 'Better Inventory', default: true, description: 'Updates the inventory layout and appearance and adds a variety of small features.', load: betterInventory },
       { id: 'better-mh-better-journal', name: 'Better Journal', default: true, description: 'Modify the journal text, layout, and styling.', load: betterJournal },
       { id: 'better-mh-quests', name: 'Better Quests', default: false, description: '', load: betterQuests },
-      { id: 'better-mh-better-shops', name: 'Better Shops', default: true, description: '', load: betterShops },
+      { id: 'better-mh-better-shops', name: 'Better Shops', default: true, description: 'Updates the Shop layout and appearance, minimizes limit 1 items that are owned, and more.', load: betterShops },
     ]
   },
   {
@@ -59,20 +59,20 @@ const modules = [
     description: 'Additional features',
     modules: [
       { id: 'better-mh-dashboard', name: 'Location Dashboard', default: true, description: 'Show a location information dashboard in the top dropdown menu.', load: dashboard },
-      { id: 'better-mh-image-upscaling', name: 'Image Upscaling', default: true, description: '', load: imageUpscaling },
+      { id: 'better-mh-image-upscaling', name: 'Image Upscaling', default: true, description: 'Uses high-res images with transparent backagrounds across the entire MH interface.', load: imageUpscaling },
       { id: 'better-mh-inventory-only-open-multiple', name: 'Inventory - Only open multiple', default: false, description: 'Lock opening things in your inventory unless you have multiple of them.', load: onlyOpenMultiple },
-      { id: 'better-mh-item-links', name: 'Item Links', default: true, description: 'Add links to MHCT, MHWiki, mhdb in item popups..', load: itemLinks },
+      { id: 'better-mh-item-links', name: 'Item Links', default: true, description: 'Add links to MHCT, MHWiki, mhdb in item popups.', load: itemLinks },
       { id: 'better-mh-mouse-links', name: 'Mouse Links', default: true, description: 'Add links to MHCT, MHWiki, mhdb in mouse popups.', load: mouseLinks },
-      { id: 'better-mh-taller-windows', name: 'Taller Windows', default: true, description: '', load: tallerWindows },
+      { id: 'better-mh-taller-windows', name: 'Taller Windows', default: true, description: 'Makes popup windows taller.', load: tallerWindows },
     ]
   },
   {
     id: 'better-mh-remove-elements',
-    name: 'Remove elements',
+    name: 'Hide Page Elements',
     modules: [
-      { id: 'better-mh-no-footer', name: 'Remove Footer', default: true, description: '', load: noFooter },
-      { id: 'better-mh-no-share', name: 'Remove Share Buttons', default: true, description: '', load: noShare },
-      { id: 'better-mh-no-sidebar', name: 'Remove Sidebar', default: false, description: '', load: noSidebar },
+      { id: 'better-mh-no-footer', name: 'Remove Footer', default: true, description: 'Hides the footer.', load: noFooter },
+      { id: 'better-mh-no-share', name: 'Remove Share Buttons', default: true, description: 'Hides the share buttons.', load: noShare },
+      { id: 'better-mh-no-sidebar', name: 'Remove Sidebar', default: false, description: 'Hides the sidebar.', load: noSidebar },
     ],
   },
   {
@@ -92,9 +92,12 @@ const modules = [
     id: 'better-mh-always-loaded',
     modules: [
       { id: 'wiki', load: wiki, alwaysLoad: true },
+
+      // move to toggleable modules
       { id: 'copy-id', load: copyId, alwaysLoad: true },
       { id: 'quick-send-supplies', load: quickSendSupplies, alwaysLoad: true },
       { id: 'testing', load: testing, alwaysLoad: true },
+      { id: 'quick-filters-and-sort', load: quickFiltersAndSort, alwaysLoad: true },
     ],
   }
 ];
