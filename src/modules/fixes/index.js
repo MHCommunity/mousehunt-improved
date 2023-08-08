d84328162c73251e0a7adc477fc65579472df49f
Brad Parbs <brad@bradparbs.com>
2023-08-08 12:11:07 -0500

    Move item classification fix into its own function

--- src/modules/fixes/index.js
+++ src/modules/fixes/index.js
@@ -1,45 +1,49 @@
 import { addUIStyles } from '../utils';
 import styles from './styles.css';
 
-const main = () => {
-  if ('item' === getCurrentPage()) {
-    const itemClassificationLink = document.querySelectorAll('.itemView-header-classification-link a');
-    if (! itemClassificationLink) {
+const updateItemClassificationLinks = () => {
+  const itemClassificationLink = document.querySelectorAll('.itemView-header-classification-link a');
+  if (! itemClassificationLink) {
+    return;
+  }
+
+  itemClassificationLink.forEach((link) => {
+    // get the onclick attribute, remove 'hg.views.ItemView.hide()', then set the onclick attribute
+    const onclick = link.getAttribute('onclick');
+    if (! onclick) {
       return;
     }
 
-    itemClassificationLink.forEach((link) => {
-      // get the onclick attribute, remove 'hg.views.ItemView.hide()', then set the onclick attribute
-      const onclick = link.getAttribute('onclick');
-      if (! onclick) {
-        return;
-      }
+    // get the page title and tab via regex
+    const page = onclick.match(/setPage\('(.+?)'.+tab:'(.+)'/);
+    if (! page) {
+      return;
+    }
 
-      // get the page title and tab via regex
-      const page = onclick.match(/setPage\('(.+?)'.+tab:'(.+)'/);
-      if (! page) {
-        return;
-      }
+    const pageTitle = page[1];
+    let tab = page[2];
+    let subtab = null;
 
-      const pageTitle = page[1];
-      let tab = page[2];
-      let subtab = null;
+    if ('skin' === tab || 'trinket' === tab) {
+      subtab = tab;
+      tab = 'traps';
+    }
 
-      if ('skin' === tab || 'trinket' === tab) {
-        subtab = tab;
-        tab = 'traps';
-      }
+    // build the url
+    let url = `https://www.mousehuntgame.com/${pageTitle.toLowerCase()}.php?tab=${tab}`;
+    if (subtab) {
+      url += `&sub_tab=${subtab}`;
+    }
 
-      // build the url
-      let url = `https://www.mousehuntgame.com/${pageTitle.toLowerCase()}.php?tab=${tab}`;
-      if (subtab) {
-        url += `&sub_tab=${subtab}`;
-      }
+    // remove the onclick attribute and add the href attribute
+    link.removeAttribute('onclick');
+    link.setAttribute('href', url);
+  });
+}
 
-      // remove the onclick attribute and add the href attribute
-      link.removeAttribute('onclick');
-      link.setAttribute('href', url);
-    });
+const main = () => {
+  if ('item' === getCurrentPage()) {
+    updateItemClassificationLinks();
   }
 };
 
