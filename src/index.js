22403808ed714184fc00e44524963735c8f80582
Brad Parbs <brad@bradparbs.com>
2023-08-08 20:18:08 -0500

    Reorder modules

--- src/index.js
+++ src/index.js
@@ -59,13 +59,15 @@ const modules = [
     name: 'UI',
     description: 'Interface and functionality improvements',
     modules: [
-      { id: 'better-ui', name: 'Better UI', default: true, description: 'Updates the MH interface with a variety of UI and style changes.', load: betterUi },
       { id: 'better-inventory', name: 'Better Inventory', default: true, description: 'Updates the inventory layout and appearance and adds a variety of small features.', load: betterInventory },
+      { id: 'better-item-view', name: 'Better Item View', default: true, description: 'Add links to MHCT & MHWiki in mouse popups as well as showing drop rates.', load: betterItemView },
       { id: 'better-journal', name: 'Better Journal', default: true, description: 'Modify the journal text, layout, and styling.', load: betterJournal },
       { id: 'better-maps', name: 'Better Maps', default: true, description: 'Updates the map layout and appearance and adds a variety of small features.', load: betterMaps },
       { id: 'better-marketplace', name: 'Better Marketplace', default: true, description: 'Updates the marketplace layout and appearance and adds a variety of small features.', load: betterMarketplace },
+      { id: 'better-mouse-view', name: 'Better Mouse View', default: true, description: 'Add links to MHCT & MHWiki in mouse popups as well as showing attraction rates.', load: betterMouseView },
       { id: 'better-send-supplies', name: 'Better Send Supplies', default: true, description: 'Adds a variety of features to the Send Supplies page.', load: betterSendSupplies, settings: betterSendSuppliesSettings },
       { id: 'better-shops', name: 'Better Shops', default: true, description: 'Updates the Shop layout and appearance, minimizes owned items that have an inventory limit of 1, and more.', load: betterShops },
+      { id: 'better-ui', name: 'Better UI', default: true, description: 'Updates the MH interface with a variety of UI and style changes.', load: betterUi },
       { id: 'quests', name: 'Better Quests', default: true, description: 'Allows you to open the assignments popup anywhere, improves the UI of the quests tab, and bundles the M400 helper.', load: betterQuests },
     ]
   },
@@ -74,8 +76,6 @@ const modules = [
     name: 'Features',
     description: 'Additional features',
     modules: [
-      { id: 'better-item-view', name: 'Better Item View', default: true, description: 'Add links to MHCT & MHWiki in mouse popups as well as showing drop rates.', load: betterItemView },
-      { id: 'better-mouse-view', name: 'Better Mouse View', default: true, description: 'Add links to MHCT & MHWiki in mouse popups as well as showing attraction rates.', load: betterMouseView },
       { id: 'copy-id', name: 'Copy ID Button', default: true, description: 'Hover over your profile picture in the HUD for a quick \'Copy ID to clipboard\' button.', load: copyId },
       { id: 'custom-shields', name: 'Custom Shield', default: false, description: 'Change your shield in the HUD to a variety of different options.', load: shields },
       { id: 'dashboard', name: 'Location Dashboard', default: true, description: 'See location HUD information in a dashboard available in the top dropdown menu.', load: dashboard },
@@ -87,12 +87,12 @@ const modules = [
       { id: 'inventory-only-open-multiple', name: 'Inventory - Only open multiple', default: false, description: 'Lock opening things in your inventory unless you have multiple of them.', load: onlyOpenMultiple },
       { id: 'journal-privacy', name: 'Journal Privacy', default: false, description: 'Hides player names in the journal.', load: journalPrivacy },
       { id: 'location-catch-stats', name: 'Location Catch Stats', default: true, description: 'Adds an item under the "Mouse" menu to see your catch stats for the current location.', load: locationHud },
+      { id: 'location-huds', name: 'Location HUD Improvements', default: true, description: 'Add additional information to the HUD for each location.', load: locationHud },
       { id: 'minluck-and-cre', name: ' Minluck & Catch Rate Estimate', default: true, description: 'Shows you the minluck and your estimated catch rate right on the camp page.', load: minluckAndCre },
       { id: 'quick-filters-and-sort', name: 'Quick Filters and Sort', default: true, description: 'Add quick filters and sorting to the trap, base, charm, and cheese selectors.', load: quickFiltersAndSort },
       { id: 'quick-send-supplies', name: 'Quick Send Supplies', default: true, description: 'Hover over the send supplies button to easily send any quantity of SUPER|brie+ or another item..', load: quickSendSupplies, settings: quickSendSuppliesSettings },
       { id: 'taller-windows', name: 'Taller Windows', default: true, description: 'Makes popup windows taller.', load: tallerWindows },
       { id: 'tem-crowns', name: 'TEM Crowns', default: true, description: 'Adds crowns and catches to the the Trap Effectiveness Meter.', load: temCrowns },
-      { id: 'location-huds', name: 'Location HUD Improvements', default: true, description: 'Add additional information to the HUD for each location.', load: locationHud },
       { id: 'travel-tweaks', name: 'Travel Tweaks', default: true, description: 'Add a variety of features to the travel page.', load: travelTweaks },
       { id: 'ultimate-checkmark', name: 'Ultimate Checkmark', default: false, description: 'Adds more things collect on the items view of your Hunter profile.', load: ultimateCheckmark },
     ]
@@ -116,7 +116,7 @@ const modules = [
   }
 ];
 
-// enableDebugMode();
+enableDebugMode();
 
 const main = () => {
   debug('Starting MH UI');
@@ -130,6 +130,8 @@ const main = () => {
         return;
       }
 
+      debug(`Adding settings for ${subModule.name}`);
+
       addSetting(
         subModule.name,
         subModule.id,
@@ -139,7 +141,10 @@ const main = () => {
         'better-mh-settings'
       );
 
+      debug(`Added settings for ${subModule.name}`);
+
       if (subModule.settings && (subModule.alwaysLoad || getSetting(subModule.id, subModule.default))) {
+        debug(`Adding settings for ${subModule.name}`);
         subModule.settings(subModule, module);
       }
     });
@@ -148,14 +153,16 @@ const main = () => {
   eventRegistry.doEvent('better-mh-before-load');
   // Load the modules.
   modules.forEach((module) => {
+    debug(`Loading module ${module.name}`);
     module.modules.forEach((subModule) => {
-      eventRegistry.doEvent(`better-mh-before-load-${subModule.id}`);
+      debug(`Loading sub-module ${subModule.name}`);
       if (subModule.alwaysLoad) {
+        debug(`Loading always-load sub-module ${subModule.name}`);
         subModule.load();
       } else if (getSetting(subModule.id, subModule.default)) {
+        debug(`Loading enabled sub-module ${subModule.name}`);
         subModule.load();
       }
-      eventRegistry.doEvent(`better-mh-after-load-${subModule.id}`);
     });
   });
 
