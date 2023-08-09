1456041fd7d088b2d63105f487ca48147fab1a44
Brad Parbs <brad@bradparbs.com>
2023-08-09 15:31:14 -0500

    Add frift to dashboard

--- src/modules/dashboard/location/furoma-rift.js
+++ src/modules/dashboard/location/furoma-rift.js
@@ -1,4 +1,30 @@
 export default (quests) => {
-  // battery
-  return quests ? 'battery info and if youre in the pagoda' : false;
+  if (! quests.QuestRiftFuroma) {
+    return '';
+  }
+
+  const q = quests.QuestRiftFuroma;
+
+  const map = {
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9,
+    ten: 10,
+  };
+
+  if (q.view_state.includes('pagoda')) {
+    const droidLevel = map[q.droid.charge_level.split('_')[2]];
+    const batteryPercent = Math.floor(q.batteries[q.droid.charge_level].percent);
+
+    return `Pagoda, Battery ${droidLevel} (${batteryPercent}%), ${q.droid.remaining_energy} energy`;
+  }
+
+  // Outside.
+  return 'Outside';
 };
