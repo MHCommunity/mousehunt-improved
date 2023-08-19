c43c24a8e98d523e7ed2f4ec3b90dc765f190f75
Brad Parbs <brad@bradparbs.com>
2023-08-18 22:26:11 -0500

    Friend search improvements

--- src/modules/better-ui/friends.js
+++ src/modules/better-ui/friends.js
@@ -36,15 +36,60 @@ const scrollToTopOnFriendsPageChange = () => {
 };
 
 const goToFriendsPageOnSearchSelect = () => {
-  const friends = $('.friendsPage-list-search');
-  friends.on('change', (e) => {
+  const friends = document.querySelector('.friendsPage-list-search');
+  if (! friends) {
+    return;
+  }
+
+  friends.addEventListener('change', (e) => {
     setTimeout(() => {
       hg.utils.PageUtil.showHunterProfile(e.val);
     }, 250);
   });
 };
 
+const goToFriendPageOnSearchID = (req) => {
+  // Make sure we're on the search page.
+  if ('community' !== getCurrentSubtab()) {
+    return;
+  }
+
+  if (! req.friend.sn_user_id) {
+    return;
+  }
+
+  hg.utils.PageUtil.showHunterProfile(req.friend.sn_user_id);
+};
+
+const reorderBlocks = () => {
+  if ('friends' !== getCurrentPage()) {
+    return;
+  }
+
+  const blocks = document.querySelectorAll('.friendsPage-community-channel');
+  if (! blocks || blocks.length < 3) {
+    return;
+  }
+
+  // Move the third block to the top and make the input bigger.
+  const block = blocks[2];
+  const parent = block.parentNode;
+  parent.removeChild(block);
+  parent.insertBefore(block, parent.firstChild);
+  block.classList.add('friends-page-id-search');
+
+  const input = block.querySelector('input');
+  if (input) {
+    // disable the 1password icon
+    input.setAttribute('data-1p-ignore', 'true');
+  }
+};
+
 export default () => {
   scrollToTopOnFriendsPageChange();
   goToFriendsPageOnSearchSelect();
+
+  onAjaxRequest(goToFriendPageOnSearchID, 'managers/ajax/pages/friends.php');
+
+  onNavigation(reorderBlocks, { page: 'friends' });
 };
