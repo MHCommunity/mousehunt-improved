aacb6d054e2dae401884bf9d9f33935340c5cc44
Brad Parbs <brad@bradparbs.com>
2023-09-13 16:24:27 -0500

    Add tournament member listing on hover

--- src/modules/better-ui/tournaments.js
+++ src/modules/better-ui/tournaments.js
@@ -22,118 +22,110 @@ const updateTournamentHud = async () => {
     return;
   }
 
-  if (! tourneyData.page?.is_active) {
-    return;
-  }
+  if (tourneyData.page?.is_active) {
+    const name = tourneyData?.page?.name;
+    if (name) {
+      activeTourney.innerText = name;
+    }
 
-  const name = tourneyData?.page?.name;
-  if (name) {
-    activeTourney.innerText = name;
-  }
+    const rank = document.querySelector('.tournamentStatusHud .rank');
+    if (rank) {
+      const scoreHover = document.createElement('div');
+      scoreHover.classList.add('scoreHover');
 
-  const rank = document.querySelector('.tournamentStatusHud .rank');
-  if (rank) {
-    const scoreHover = document.createElement('div');
-    scoreHover.classList.add('scoreHover');
+      tourneyData.page.scoreboard.rows.forEach((scoreboard) => {
+        const scoreRow = makeElement('div', 'scoreRow');
 
-    tourneyData.page.scoreboard.rows.forEach((scoreboard) => {
-      const scoreRow = document.createElement('div');
-      scoreRow.classList.add('scoreRow');
+        makeElement('div', 'scoreRank', scoreboard.rank, scoreRow);
 
-      const rankText = document.createElement('div');
-      rankText.classList.add('scoreRank');
-      rankText.innerText = scoreboard.rank;
+        const teamWrapper = makeElement('a', 'teamWrapper');
+        teamWrapper.href = `https://www.mousehuntgame.com/team.php?team_id=${scoreboard.team_id}`;
 
-      scoreRow.appendChild(rankText);
+        const icon = makeElement('div', 'scoreIcon');
 
-      const teamWrapper = document.createElement('a');
-      teamWrapper.classList.add('teamWrapper');
-      teamWrapper.href = `https://www.mousehuntgame.com/team.php?team_id=${scoreboard.team_id}`;
+        const iconLayer1 = makeElement('div', 'scoreIconLayer1');
+        iconLayer1.style.backgroundImage = `url(${scoreboard.emblem.layers[0].image})`;
+        icon.appendChild(iconLayer1);
 
-      const icon = document.createElement('div');
-      icon.classList.add('scoreIcon');
+        const iconLayer2 = makeElement('div', 'scoreIconLayer2');
+        iconLayer2.style.backgroundImage = `url(${scoreboard.emblem.layers[1].image})`;
+        icon.appendChild(iconLayer2);
 
-      const iconLayer1 = document.createElement('div');
-      iconLayer1.classList.add('scoreIconLayer1');
-      iconLayer1.style.backgroundImage = `url(${scoreboard.emblem.layers[0].image})`;
-      icon.appendChild(iconLayer1);
+        const iconLayer3 = makeElement('div', 'scoreIconLayer3');
+        iconLayer3.style.backgroundImage = `url(${scoreboard.emblem.layers[2].image})`;
+        icon.appendChild(iconLayer3);
 
-      const iconLayer2 = document.createElement('div');
-      iconLayer2.classList.add('scoreIconLayer2');
-      iconLayer2.style.backgroundImage = `url(${scoreboard.emblem.layers[1].image})`;
-      icon.appendChild(iconLayer2);
+        teamWrapper.appendChild(icon);
 
-      const iconLayer3 = document.createElement('div');
-      iconLayer3.classList.add('scoreIconLayer3');
-      iconLayer3.style.backgroundImage = `url(${scoreboard.emblem.layers[2].image})`;
-      icon.appendChild(iconLayer3);
+        makeElement('div', 'scoreName', scoreboard.name, teamWrapper);
 
-      teamWrapper.appendChild(icon);
+        scoreRow.appendChild(teamWrapper);
 
-      const scoreName = document.createElement('div');
-      scoreName.classList.add('scoreName');
-      scoreName.innerText = scoreboard.name;
+        makeElement('div', 'scorePoints', scoreboard.points, scoreRow);
 
-      teamWrapper.appendChild(scoreName);
-
-      scoreRow.appendChild(teamWrapper);
-
-      const points = document.createElement('div');
-      points.classList.add('scorePoints');
-      points.innerText = scoreboard.points;
-
-      scoreRow.appendChild(points);
-
-      scoreHover.appendChild(scoreRow);
-    });
-
-    rank.appendChild(scoreHover);
-  }
-
-  const points = document.querySelector('.tournamentStatusHud .score');
-  if (points) {
-    const pointsHover = document.createElement('div');
-    pointsHover.classList.add('pointsHover');
-
-    // reverse the tourneyData.mouse_groups array and loop through it.
-    tourneyData.page.mouse_groups.reverse().forEach((mouseGroup) => {
-      const pointsRow = document.createElement('div');
-      pointsRow.classList.add('pointsRow');
-
-      const groupTitle = document.createElement('div');
-      groupTitle.classList.add('pointsTitle');
-      groupTitle.innerText = mouseGroup.name;
-
-      pointsRow.appendChild(groupTitle);
-
-      const groupMice = document.createElement('div');
-      groupMice.classList.add('pointsMice');
-
-      mouseGroup.mice.forEach((mouse) => {
-        const mouseWrapper = document.createElement('div');
-        mouseWrapper.classList.add('pointsMouseWrapper');
-
-        const mouseIcon = document.createElement('img');
-        mouseIcon.classList.add('pointsMouseIcon');
-        mouseIcon.src = mouse.thumb;
-
-        mouseWrapper.appendChild(mouseIcon);
-
-        const mouseName = document.createElement('div');
-        mouseName.classList.add('pointsMouseName');
-        mouseName.innerText = mouse.name;
-
-        mouseWrapper.appendChild(mouseName);
-
-        groupMice.appendChild(mouseWrapper);
+        scoreHover.appendChild(scoreRow);
       });
 
-      pointsRow.appendChild(groupMice);
+      rank.appendChild(scoreHover);
+    }
 
-      pointsHover.appendChild(pointsRow);
-    });
+    const points = document.querySelector('.tournamentStatusHud .score');
+    if (points) {
+      const pointsHover = document.createElement('div');
+      pointsHover.classList.add('pointsHover');
 
-    points.appendChild(pointsHover);
+      // reverse the tourneyData.mouse_groups array and loop through it.
+      tourneyData.page.mouse_groups.reverse().forEach((mouseGroup) => {
+        const pointsRow = makeElement('div', 'pointsRow');
+
+        makeElement('div', 'pointsTotal', mouseGroup.name, pointsRow);
+
+        const groupMice = document.createElement('div');
+        groupMice.classList.add('pointsMice');
+
+        mouseGroup.mice.forEach((mouse) => {
+          const mouseWrapper = makeElement('div', 'pointsMouseWrapper');
+
+          const mouseIcon = makeElement('img', 'pointsMouseIcon');
+          mouseIcon.src = mouse.thumb;
+
+          mouseWrapper.appendChild(mouseIcon);
+
+          makeElement('div', 'pointsMouseName', mouse.name, mouseWrapper);
+
+          groupMice.appendChild(mouseWrapper);
+        });
+
+        pointsRow.appendChild(groupMice);
+        pointsHover.appendChild(pointsRow);
+      });
+
+      points.appendChild(pointsHover);
+    }
+  } else {
+    const members = document.querySelector('.tournamentStatusHud a.teamMembers');
+    if (members) {
+      const memberHover = makeElement('div', 'memberHover');
+
+      tourneyData.page.members.forEach((member) => {
+        const memberRow = makeElement('div', 'memberRow');
+
+        if (member.is_empty) {
+          makeElement('div', 'memberEmpty', 'Empty', memberRow);
+          memberRow.classList.add('empty');
+        } else {
+          const image = makeElement('img', 'memberImage');
+          image.src = member.profile_pic ? member.profile_pic : 'https://www.mousehuntgame.com//images/ui/friends/anonymous_user.png';
+          memberRow.appendChild(image);
+
+          makeElement('div', 'memberName', member.name ? member.name : '', memberRow);
+        }
+
+        memberHover.appendChild(memberRow);
+      });
+
+      members.appendChild(memberHover);
+    }
   }
 };
 
