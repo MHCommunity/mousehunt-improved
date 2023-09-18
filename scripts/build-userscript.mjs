b78aff071c4b2c69e26246a8081a547b71fd9c07
Brad Parbs <brad@bradparbs.com>
2023-09-18 16:24:19 -0500

    Update esbuild for userscript

--- scripts/build-userscript.mjs
+++ scripts/build-userscript.mjs
@@ -2,7 +2,7 @@ import * as esbuild from 'esbuild';
 import fs from 'fs';
 import path from 'path';
 
-await esbuild.buildSync({
+await esbuild.build({
   entryPoints: ['src/index.js'],
   outfile: 'dist/mousehunt-improved.user.js',
   banner: {
@@ -19,6 +19,7 @@ await esbuild.buildSync({
   metafile: true,
   sourcemap: true,
   splitting: false,
+  logLevel: 'info',
   define: {
     EXT_VERSION: JSON.stringify(process.env.npm_package_version),
   }
