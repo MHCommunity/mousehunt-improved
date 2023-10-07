5e1c7c0483b13f502156771306017ce83fe9f3a0
Brad Parbs <brad@bradparbs.com>
2023-10-06 21:09:06 -0500

    Add export/import buttons to rankup forecast userscript

--- src/modules/_required/index.js
+++ src/modules/_required/index.js
@@ -3,6 +3,7 @@ import globalStyles from './global-styles.css';
 import fixesStyles from './fixes.css';
 import darkmodeStyles from './darkmode.css';
 import settingsStyles from './settings.css';
+import moveToModuleStyles from './move-to-module.css';
 
 const updateItemClassificationLinks = () => {
   const itemClassificationLink = document.querySelectorAll('.itemView-header-classification-link a');
@@ -113,12 +114,99 @@ const continueOnKingsReward = (req) => {
   }
 };
 
+const exportRankupForecasterData = () => {
+  const allArea = localStorage.getItem('Chro-forecaster-all-area');
+  const currentArea = localStorage.getItem('Chro-forecaster-current-area');
+  const time = localStorage.getItem('Chro-forecaster-time');
+
+  const data = {
+    allArea,
+    currentArea,
+    time,
+  };
+
+  const dataStr = JSON.stringify(data);
+
+  // encode the data to base64
+  const base64 = btoa(dataStr);
+
+  // create the link
+  const link = document.createElement('a');
+  const date = new Date();
+  const dateString = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+  link.download = `rank-up-forecaster-${dateString}.json`;
+  link.href = `data:application/json;base64,${base64}`;
+  link.click();
+};
+
+const importRankupForecassterData = () => {
+  const input = document.createElement('input');
+  input.type = 'file';
+  input.accept = '.json';
+
+  input.addEventListener('change', (e) => {
+    const file = e.target.files[0];
+    if (! file) {
+      return;
+    }
+
+    const reader = new FileReader();
+    reader.onload = (re) => {
+      const contents = re.target.result;
+      const data = JSON.parse(contents);
+
+      localStorage.setItem('Chro-forecaster-all-area', data.allArea);
+      localStorage.setItem('Chro-forecaster-current-area', data.currentArea);
+      localStorage.setItem('Chro-forecaster-time', data.time);
+    };
+
+    reader.readAsText(file);
+  });
+
+  input.click();
+
+  // re-click the points button to refresh the data
+  const points = document.querySelector('.mousehuntHud-userStat-row.points');
+  if (points) {
+    points.click();
+  }
+};
+
+const addRankupForecasterButtons = () => {
+  const forecastOpen = document.querySelector('.mousehuntHud-userStat-row.points');
+  if (! forecastOpen) {
+    return;
+  }
+
+  forecastOpen.addEventListener('click', () => {
+    setTimeout(() => {
+      const rankup = document.querySelector('#forecaster-content-div');
+      if (! rankup) {
+        return;
+      }
+
+      const wrapper = makeElement('div', 'mh-ui-forecaster-buttons');
+
+      const exportButton = makeElement('button', 'mh-ui-export-forecaster-data', 'Export Data');
+      exportButton.addEventListener('click', exportRankupForecasterData);
+      wrapper.appendChild(exportButton);
+
+      const importButton = makeElement('button', 'mh-ui-import-forecaster-data', 'Import Data');
+      importButton.addEventListener('click', importRankupForecassterData);
+      wrapper.appendChild(importButton);
+
+      rankup.appendChild(wrapper);
+    }, 250);
+  });
+};
+
 export default () => {
   addUIStyles([
     globalStyles,
     fixesStyles,
     darkmodeStyles,
     settingsStyles,
+    moveToModuleStyles,
   ].join('\n'));
 
   if ('item' === getCurrentPage()) {
@@ -132,4 +220,6 @@ export default () => {
   onRequest(addDarkModeBodyClass);
 
   onRequest(continueOnKingsReward, 'managers/ajax/users/puzzle.php', true);
+
+  addRankupForecasterButtons();
 };
