40c1ecd1879e8cf680e3d19131189c9982ffc353
Brad Parbs <brad@bradparbs.com>
2023-10-07 23:07:08 -0500

    Add styles for specific sorted maps

--- src/modules/better-maps/styles/index.js
+++ src/modules/better-maps/styles/index.js
@@ -6,6 +6,7 @@ import mapArea from './map-ar.css';
 import mhct from './mhct.css';
 import sidebar from './sidebar.css';
 import sorted from './sorted.css';
+import sortedMap from './sorted-map.css';
 
 const main = () => {
   addUIStyles([
@@ -15,6 +16,7 @@ const main = () => {
     mhct,
     sidebar,
     sorted,
+    sortedMap,
   ].join('\n'));
 };
 
