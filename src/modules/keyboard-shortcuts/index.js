fac93b2a5df8bb817bd3643656821e0b00b68ea6
Brad Parbs <brad@bradparbs.com>
2023-10-08 13:31:35 -0500

    Reorder keyboard shortcuts

--- src/modules/keyboard-shortcuts/index.js
+++ src/modules/keyboard-shortcuts/index.js
@@ -74,11 +74,11 @@ const listenForKeypresses = () => {
     case 'j': hg.utils.PageUtil.setPage('Camp'); break; // J for Journal/camp.
     case 'l': clickMinLuck(); break; // L for Luck.
     case 'm': hg.controllers.TreasureMapController.show(); break; // M for map.
+    case 'p': hg.utils.PageUtil.setPage('HunterProfile'); break; // P for Profile.
     case 'r': app.pages.CampPage.toggleItemBrowser('trinket'); break; // r for chaRm.
     case 's': hg.utils.PageUtil.setPage('Shops'); break; // S for shop.
     case 't': hg.utils.PageUtil.setPage('Travel'); break; // T for travel.
     case 'w': app.pages.CampPage.toggleItemBrowser('weapon'); break; // W for Weapon.
-    case 'p': hg.utils.PageUtil.setPage('HunterProfile'); break; // P for Profile.
     }
   });
 };
