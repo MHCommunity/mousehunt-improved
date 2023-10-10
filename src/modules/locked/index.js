acc56f8557e148f8148720af6a8f9027ec728df7
Brad Parbs <brad@bradparbs.com>
2023-10-10 00:02:49 -0500

    remove console log

--- src/modules/locked/index.js
+++ src/modules/locked/index.js
@@ -2,7 +2,6 @@ import { getFlag } from '../utils';
 import trollMode from './modules/troll-mode';
 
 export default () => {
-  console.log('lol gottem', getFlag('lol-gottem'));
   if (getFlag('lol-gottem')) {
     trollMode();
   }
