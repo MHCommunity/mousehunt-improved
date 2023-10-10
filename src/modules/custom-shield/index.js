894b5ffeee22b1caf858977d6135b3bdf02e77a7
Brad Parbs <brad@bradparbs.com>
2023-10-09 21:12:08 -0500

    Refactor custom shields, improve title shield display, add morre options

--- src/modules/custom-shield/index.js
+++ src/modules/custom-shield/index.js
@@ -10,11 +10,6 @@ const addClass = (el, shieldClass) => {
 };
 
 const changeShield = () => {
-  const shield = getMhuiSetting('custom-shield-0', 'default');
-  if ('default' === shield) {
-    return;
-  }
-
   const shieldEl = document.querySelector('.mousehuntHud-shield');
   if (! shieldEl) {
     return;
@@ -27,14 +22,51 @@ const changeShield = () => {
     }
   });
 
-  addClass(shieldEl, `${shield}-shield`);
-
-  if ('title' === shield) {
-    shieldEl.style.backgroundImage = `url(${titleBadge()})`;
+  const timer = document.querySelector('.huntersHornView__timer--default');
+  if (timer) {
+    // remove all the classes that start with 'color-' from the timer.
+    timer.classList.forEach((className) => {
+      if (className.startsWith('color-')) {
+        timer.classList.remove(className);
+      }
+    });
   }
+
+  let shield = getMhuiSetting('custom-shield-0', 'default');
+  if ('default' === shield) {
+    shieldEl.classList.add('default');
+    return;
+  }
+
+  if (shield.startsWith('color-')) {
+    shieldEl.classList.add('default');
+  }
+
+  if (shield.endsWith('-timer')) {
+    shield = shield.replace('-timer', '');
+    if (timer) {
+      timer.classList.add(shield);
+    }
+  }
+
+  if (shield.endsWith('-alt')) {
+    // if its the alt, also add the non-alt class.
+    shieldEl.classList.add(shield.replace('-alt', ''), 'alt');
+  }
+
+  if (shield.includes('title')) {
+    shieldEl.classList.add('title');
+
+    if ('title' === shield) {
+      shield = getTitle();
+    }
+  }
+
+  shieldEl.classList.add('mhui-custom-shield');
+  addClass(shieldEl, shield);
 };
 
-const titleBadge = () => {
+const getTitle = () => {
   let title = user.title_name || 'novice';
   title = title.toLowerCase();
 
@@ -43,29 +75,7 @@ const titleBadge = () => {
   title.replace('ess', '');
   title.replace('duch', 'duke');
 
-  const titleImages = {
-    novice: 'https://www.mousehuntgame.com/images/titles/84bc1109b5cd7aa8c24d195bc8207c38.png?cv=2',
-    apprentice: 'https://www.mousehuntgame.com/images/titles/3f1e44bbaa7138da4c326819e9f3f0a8.png?cv=2',
-    initiate: 'https://www.mousehuntgame.com/images/titles/6f4673dd2d9d1e98b4569667d702a775.png?cv=2',
-    journeyman: 'https://www.mousehuntgame.com/images/titles/e96387f7261b95c0eeab9291e4e594e1.png?cv=2',
-    master: 'https://www.mousehuntgame.com/images/titles/ad6875955f541159133c6d3798519f81.png?cv=2',
-    grandmaster: 'https://www.mousehuntgame.com/images/titles/35ee6056a09037fb13a9195881875045.png?cv=2',
-    legendary: 'https://www.mousehuntgame.com/images/titles/0da3761747914f497c16dc2051ba132d.png?cv=2',
-    hero: 'https://www.mousehuntgame.com/images/titles/fca35751046f4bcc972716ca484b6d61.png?cv=2',
-    knight: 'https://www.mousehuntgame.com/images/titles/398dca9a8c7703de969769491622ca32.png?cv=2',
-    lord: 'https://www.mousehuntgame.com/images/titles/9a6acd429a9a3a4849ed13901288b0b8.png?cv=2',
-    baron: 'https://www.mousehuntgame.com/images/titles/ea9c0ec2e6d3d81c14e61f5ce924d0e1.png?cv=2',
-    count: 'https://www.mousehuntgame.com/images/titles/dd11711a25b80db90e0306193f2e8d78.png?cv=2',
-    duke: 'https://www.mousehuntgame.com/images/titles/eb46ac1e8197b13299ab860f07d963db.png?cv=2',
-    grandduke: 'https://www.mousehuntgame.com/images/titles/87937fa96bbb3b2dd3225df883002642.png?cv=2',
-    archduke: 'https://www.mousehuntgame.com/images/titles/043efe31de4f0f2e0ddca590fe829032.png?cv=2',
-    viceroy: 'https://www.mousehuntgame.com/images/titles/e2e79f6f9201a4d4e7a89684fbb5356f.png?cv=2',
-    elder: 'https://www.mousehuntgame.com/images/titles/0f3cf224bf98457f6b5bad91ab1c7bd2.png?cv=2',
-    sage: 'https://www.mousehuntgame.com/images/titles/cb49e43c5e4460da7c09fe28ca4f44ce.png?cv=2',
-    fabled: 'https://www.mousehuntgame.com/images/titles/5daba92a8d609834aa8b789f37544e08.png?cv=2',
-  };
-
-  return titleImages[title];
+  return title;
 };
 
 const changeShieldColor = () => {
@@ -74,16 +84,14 @@ const changeShieldColor = () => {
     return;
   }
 
-  const classes = [
-    'a.mousehuntHud-shield.golden'
-  ];
+  let classes = 'a.mousehuntHud-shield.golden';
 
   const matchTimer = localStorage.getItem('custom-shield-timer');
   if (matchTimer) {
-    classes.push('.huntersHornView__timer--default');
+    classes += ' .huntersHornView__timer--default';
   }
 
-  addUIStyles(` ${classes.join(', ')}{ filter: hue-rotate(${color}deg); }`);
+  addUIStyles(` ${classes}{ filter: hue-rotate(${color}deg); }`);
 };
 
 export default () => {
@@ -92,4 +100,18 @@ export default () => {
   changeShield();
 
   changeShieldColor();
+
+  onNavigation(() => {
+    const input = document.querySelector('#mousehunt-improved-settings-feature-modules-custom-shield select');
+    if (! input) {
+      return;
+    }
+
+    input.addEventListener('change', () => {
+      changeShield();
+    });
+  }, {
+    page: 'preferences',
+    onLoad: true,
+  });
 };
--- src/modules/custom-shield/settings/index.js
+++ src/modules/custom-shield/settings/index.js
@@ -1,86 +1,83 @@
 export default function (subModule, module) {
-  const options = [
-    {
-      name: 'Default',
-      value: 'default',
-    },
-    {
-      name: 'Birthday',
-      value: 'birthday',
-    },
-    {
-      name: 'Birthday (Year 10)',
-      value: 'birthday.year10',
-    },
-    {
-      name: 'Birthday (Year 11)',
-      value: 'birthday.year11',
-    },
-    {
-      name: 'Birthday (Year 12)',
-      value: 'birthday.year12',
-    },
-    {
-      name: 'Birthday (Year 13)',
-      value: 'birthday.year13',
-    },
-    {
-      name: 'Birthday (Year 14)',
-      value: 'birthday.year14',
-    },
-    {
-      name: 'Birthday (Year 15)',
-      value: 'birthday.year15',
-    },
-    {
-      name: 'Halloween',
-      value: 'halloween',
-    },
-    {
-      name: 'Remembrance Day',
-      value: 'remembrance_day',
-    },
-    {
-      name: 'Valentine\'s',
-      value: 'valentines',
-    },
-    {
-      name: 'Great Winter Hunter',
-      value: 'winter_hunt',
-    },
-    {
-      name: 'Larry\'s Football Challenge',
-      value: 'larrys_football_challenge',
-    },
-    {
-      name: 'Title ',
-      value: 'title',
-    },
-    {
-      name: 'Fabled ',
-      value: 'fabled',
-    },
-    {
-      name: 'Scrambles ',
-      value: 'scrambles',
-    },
-    {
-      name: 'Jerry ',
-      value: 'jerry',
-    },
-    {
-      name: 'Romeno ',
-      value: 'romeno',
-    },
-    {
-      name: 'Captain America',
-      value: 'capt-america',
-    },
-    {
-      name: 'Hylian Shield',
-      value: 'hylian',
-    },
+  let options = [
+    { name: 'Default', value: 'default' },
+    { name: 'Events', value: 'group', options: [
+      { name: 'Birthday (Year 10)', value: 'birthday.year10' },
+      { name: 'Birthday (Year 11)', value: 'birthday.year11' },
+      { name: 'Birthday (Year 12)', value: 'birthday.year12' },
+      { name: 'Birthday (Year 13)', value: 'birthday.year13' },
+      { name: 'Birthday (Year 14)', value: 'birthday.year14' },
+      { name: 'Birthday (Year 15)', value: 'birthday.year15' },
+      { name: 'Birthday', value: 'birthday' },
+      { name: 'Great Winter Hunter', value: 'winter_hunt' },
+      { name: 'Halloween', value: 'halloween' },
+      { name: 'Larry\'s Football Challenge', value: 'larrys_football_challenge' },
+      { name: 'Pride', value: 'pride' },
+      { name: 'Remembrance Day', value: 'remembrance_day' },
+      { name: 'Spring Egg Hunt', value: 'spring-egg-hunt' },
+      { name: 'Spring Egg Hunt (Alternate)', value: 'spring-egg-hunt-alt' },
+      { name: 'Valentine\'s', value: 'valentines' },
+    ] },
+    { name: 'Color', value: 'group', options: [
+      { name: 'Blue', value: 'color-blue' },
+      { name: 'Blue with matching timer', value: 'color-blue-timer' },
+      { name: 'Cyan', value: 'color-cyan' },
+      { name: 'Cyan with matching timer', value: 'color-cyan-timer' },
+      { name: 'Green', value: 'color-green' },
+      { name: 'Green with matching timer', value: 'color-green-timer' },
+      { name: 'Pink', value: 'color-pink' },
+      { name: 'Pink with matching timer', value: 'color-pink-timer' },
+      { name: 'Purple', value: 'color-purple' },
+      { name: 'Purple with matching timer', value: 'color-purple-timer' },
+      { name: 'Red', value: 'color-red' },
+      { name: 'Red with matching timer', value: 'color-red-timer' },
+      { name: 'Faded', value: 'color-faded' },
+      { name: 'Rainbow', value: 'color-rainbow' },
+      { name: 'Rainbow with matching timer', value: 'color-rainbow2-timer' },
+    ] },
+    { name: 'Title Shields', value: 'group', options: [
+      { name: 'Current Title ', value: 'title' },
+      { name: 'Novice', value: 'title.novice' },
+      { name: 'Recruit', value: 'title.recruit' },
+      { name: 'Apprentice', value: 'title.apprentice' },
+      { name: 'Initiate', value: 'title.initiate' },
+      { name: 'Journeyman / Journeywoman', value: 'title.journeyman' },
+      { name: 'Master', value: 'title.master' },
+      { name: 'Grandmaster', value: 'title.grandmaster' },
+      { name: 'Legendary', value: 'title.legendary' },
+      { name: 'Hero', value: 'title.hero' },
+      { name: 'Knight', value: 'title.knight' },
+      { name: 'Lord / Lady', value: 'title.lord' },
+      { name: 'Baron / Baroness', value: 'title.baron' },
+      { name: 'Count / Countess', value: 'title.count' },
+      { name: 'Duke / Duchess', value: 'title.duke' },
+      { name: 'Grand Duke / Duchess', value: 'title.grandduke' },
+      { name: 'Archduke / Archduchess', value: 'title.archduke' },
+      { name: 'Viceroy', value: 'title.viceroy' },
+      { name: 'Elder', value: 'title.elder' },
+      { name: 'Sage', value: 'title.sage' },
+      { name: 'Fabled', value: 'title.fabled' },
+    ] },
+    { name: 'Silly', value: 'group', options: [
+      { name: 'Scrambles ', value: 'scrambles' },
+      { name: 'Jerry ', value: 'jerry' },
+      { name: 'Romeno ', value: 'romeno' }
+    ] }
   ];
+
+  if (! user.has_shield) {
+    // If the user doesn't have LGS, they can't do the pride or SEH shields.
+    const toRemove = [
+      'pride',
+      'spring-egg-hunt',
+      'spring-egg-hunt-alt',
+
+    ];
+
+    options = options.filter((option) => {
+      return ! toRemove.includes(option.value);
+    });
+  }
 
   addSetting(
     'Custom Shield',
