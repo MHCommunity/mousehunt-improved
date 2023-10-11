43f679f6c6171231f0c9601cbc4c85a0d936eb62
Brad Parbs <brad@bradparbs.com>
2023-10-11 08:14:50 -0500

    remove background page

--- src/extension/background.js
+++
@@ -1,7 +0,0 @@
-chrome.runtime.onInstalled.addListener((details) => {
-  if (details.reason === 'install') {
-    chrome.tabs.create({ url: 'https://www.mousehuntgame.com/' });
-  } else if (details.reason === 'update') {
-    // chrome.tabs.create({ url: `https://github.com/MHCommunity/mousehunt-improved/releases/tag/v${chrome.runtime.getManifest().version}` });
-  }
-});
