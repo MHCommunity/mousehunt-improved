4d3401aa7bc22429afb5b2a46c80d79b4277f2ca
Brad Parbs <brad@bradparbs.com>
2023-10-11 08:14:34 -0500

    Bump mh-utils version

--- src/userscript-header.js
+++ src/userscript-header.js
@@ -9,5 +9,5 @@
 // @icon        https://i.mouse.rip/mouse.png
 // @run-at      document-end
 // @grant       none
-// @require     https://cdn.jsdelivr.net/npm/mousehunt-utils@1.10.5/mousehunt-utils.js
+// @require     https://cdn.jsdelivr.net/npm/mousehunt-utils@1.11.0/mousehunt-utils.js
 // ==/UserScript==
