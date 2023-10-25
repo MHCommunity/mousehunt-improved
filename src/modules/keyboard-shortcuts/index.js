791b0c11e4cd587b8cccc69423db0413b0815eca
Brad Parbs <brad@bradparbs.com>
2023-10-25 14:02:43 -0500

    Fix syntax error

--- src/modules/keyboard-shortcuts/index.js
+++ src/modules/keyboard-shortcuts/index.js
@@ -54,7 +54,7 @@ const listenForKeypresses = () => {
       document.activeElement instanceof HTMLTextAreaElement || // eslint-disable-line @wordpress/no-global-active-element
       document.activeElement instanceof HTMLSelectElement || // eslint-disable-line @wordpress/no-global-active-element
       event.metaKey || // if the meta key is pressed, we don't want to do anything.
-      event.ctrlKey || // if the ctrl key is pressed, we don't want to do anything.
+      event.ctrlKey // if the ctrl key is pressed, we don't want to do anything.
     ) {
       return;
     }
