cb09f0894a899cf9119014ea78f6e3cb68d9a02a
Brad Parbs <brad@bradparbs.com>
2023-11-01 13:05:14 -0500

    Re-enable halloween cheese arm buttons

--- src/modules/location-hud/halloween-event-location/index.js
+++ src/modules/location-hud/halloween-event-location/index.js
@@ -1,8 +1,26 @@
 import { addHudStyles } from '../../utils';
 import styles from './styles.css';
 
+const undisableCheese = () => {
+  const armButtons = document.querySelectorAll('.halloweenBoilingCauldronHUD-bait');
+  armButtons.forEach((armButton) => {
+    armButton.classList.remove('disabled');
+    const link = armButton.querySelector('a.disabled');
+    if (link) {
+      link.classList.remove('disabled');
+    }
+
+    const tooltipLink = armButton.querySelector('.halloweenBoilingCauldronHUD-bait-tooltipContent a.disabled');
+    if (tooltipLink) {
+      tooltipLink.classList.remove('disabled');
+    }
+  });
+};
+
 const main = () => {
   addHudStyles('halloween-event-location', styles);
+  undisableCheese();
+  onRequest(undisableCheese);
 };
 
 export default main;
