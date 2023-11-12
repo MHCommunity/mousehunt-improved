6ea822884d424e99400a796bcc49fb0839e55330
Brad Parbs <brad@bradparbs.com>
2023-11-12 01:11:50 -0600

    Set update notifications time to persist correctly

--- src/modules/update-notifications/index.js
+++ src/modules/update-notifications/index.js
@@ -1,37 +1,27 @@
-const getReadUpdateNotification = (version) => {
-  return getSetting(version, false, 'mousehunt-improved-update-notifications');
-};
+const getReadUpdateNotificationTime = (version) => {
+  let date = getSetting(version, false, 'mh-improved-update-notifications');
+  console.log(date);
+  if (! date) {
+    date = new Date().toISOString().slice(0, 19).replace('T', ' ');
+    saveSetting(version, date, 'mh-improved-update-notifications');
+  }
 
-const setReadUpdateNotification = (version) => {
-  return saveSetting(version, true, 'mousehunt-improved-update-notifications');
-};
-
-const addMessageToInbox = (version) => {
-  // get the date in this format "2023-11-01 05:34:15".
-  const date = new Date().toISOString().slice(0, 19).replace('T', ' ');
-  const link = `https://github.com/MHCommunity/mousehunt-improved/releases/tag/${version}`;
-
-  const m = new MessengerUINotification();
-  m.addMessage({
-    messageId: `mhui-update-notification-${version}`,
-    messageType: 'news',
-    messageData: {
-      text: `<img src="https://i.mouse.rip/mh-improved/icon-64.png" class="item" alt="MouseHunt Improved icon">MH Improved has been updated to v${version}! <a href="${link}">Check out the changelog</a> for more details.`,
-      href: link,
-      cssClass: 'mhui-notification',
-    },
-    messageDate: date,
-    isNew: true
-  });
-
-  setReadUpdateNotification(version);
+  return date;
 };
 
 export default () => {
-  if (getReadUpdateNotification(mhImprovedVersion)) {
-    return;
-  }
+  const link = `https://github.com/MHCommunity/mousehunt-improved/releases/tag/${mhImprovedVersion}`;
 
-  addMessageToInbox(mhImprovedVersion);
-  setReadUpdateNotification(mhImprovedVersion);
+  const m = new MessengerUINotification();
+  m.addMessage({
+    messageId: `mhui-update-notification-${mhImprovedVersion}`,
+    messageType: 'news',
+    messageData: {
+      text: `<img src="https://i.mouse.rip/mh-improved/icon-64.png" class="item" alt="MouseHunt Improved icon">MH Improved has been updated to v${mhImprovedVersion}! <a href="${link}">Check out the changelog</a> for more details.`,
+      href: link,
+      cssClass: 'mhui-notification',
+    },
+    messageDate: getReadUpdateNotificationTime(mhImprovedVersion),
+    isNew: true
+  });
 };
