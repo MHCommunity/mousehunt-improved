39cb2fe58b0aa532f4b8e36c5a1f27fa08006290
Brad Parbs <brad@bradparbs.com>
2023-11-13 14:48:59 -0600

    Move large skin images to better UI

--- src/modules/better-ui/index.js
+++ src/modules/better-ui/index.js
@@ -1,9 +1,11 @@
 import styles from './styles';
 import friends from './friends';
 import hud from './hud';
+import skins from './skins';
 
 export default () => {
   styles();
   friends();
   hud();
+  skins();
 };
