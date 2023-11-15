1e6a0ca899d0b67ec51e7a40fd2ce57c9e5e5e54
Brad Parbs <brad@bradparbs.com>
2023-11-15 13:23:36 -0600

    Add replaceInTemplate util

--- src/modules/utils.js
+++ src/modules/utils.js
@@ -679,6 +679,22 @@ const getRelicHunterLocation = () => {
     });
 };
 
+/**
+ * Find and replace strings in a template file.
+ *
+ * @param {string} templateId   ID of the template to replace in.
+ * @param {Array}  replacements Array of replacements to make.
+ */
+const replaceInTemplate = (templateId, replacements) => {
+  let templateContent = hg.utils.TemplateUtil.getTemplate(templateId);
+
+  replacements.forEach((replacement) => {
+    templateContent = templateContent.replace(replacement[0], replacement[1]);
+  });
+
+  hg.utils.TemplateUtil.addTemplate(templateId, templateContent);
+};
+
 export {
   addArDataToMap,
   addBodyClass,
@@ -707,5 +723,6 @@ export {
   persistBodyClass,
   removeHudStyles,
   showErrorMessage,
-  showSuccessMessage
+  showSuccessMessage,
+  replaceInTemplate
 };
