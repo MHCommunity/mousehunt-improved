e57882663af74460f160d13d42269111f423939a
Brad Parbs <brad@bradparbs.com>
2023-11-15 13:22:44 -0600

    Actually add marketplace settings

---
+++ src/modules/better-marketplace/settings/index.js
@@ -0,0 +1,11 @@
+import { addMhuiSetting } from '../../utils';
+
+export default function (module) {
+  addMhuiSetting(
+    'better-marketplace-search-all',
+    'Default to showing all items in the marketplace search.',
+    false,
+    'If disabled, then useless items will be hidden from the search dropdown by default.',
+    module
+  );
+}
