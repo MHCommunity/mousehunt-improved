d638047b05d4182d563ef6211ad4e9ee28b65a2a
Brad Parbs <brad@bradparbs.com>
2023-12-07 18:02:15 -0600

    Add 'show spoiler' button to advent calendar

--- src/modules/location-hud/region-events/index.js
+++ src/modules/location-hud/region-events/index.js
@@ -6,6 +6,42 @@ import gwh from './great-winter-hunt';
 import halloween from './halloween';
 
 
+const adventCalendarPopup = () => {
+  const suffix = document.querySelector('#overlayPopup .suffix');
+  if (! suffix) {
+    return;
+  }
+
+  const existingToggle = document.querySelector('.toggle-advent-calendar-spoilers');
+  if (existingToggle) {
+    return;
+  }
+
+  const toggleBtn = makeElement('button', ['mousehuntActionButton', 'tiny', 'toggle-advent-calendar-spoilers']);
+  makeElement('span', '', 'View unblurred calendar', toggleBtn);
+  toggleBtn.setAttribute('data-enabled', 'false');
+
+  toggleBtn.addEventListener('click', () => {
+    const popup = document.querySelector('#overlayPopup');
+    if (! popup) {
+      return;
+    }
+
+    popup.classList.toggle('advent-calendar-spoilers');
+
+    const enabled = toggleBtn.getAttribute('data-enabled');
+    if ('true' === enabled) {
+      toggleBtn.setAttribute('data-enabled', 'false');
+      toggleBtn.querySelector('span').innerText = 'View unblurred calendar';
+    } else {
+      toggleBtn.setAttribute('data-enabled', 'true');
+      toggleBtn.querySelector('span').innerText = 'Hide unblurred calendar';
+    }
+  });
+
+  suffix.append(toggleBtn);
+};
+
 export default (location) => {
   switch (location) {
   case 'halloween_event_location':
@@ -23,4 +59,5 @@ export default (location) => {
   // Need to fire it always because the showdown styles are always loaded
   addUIStyles(styles);
 
+  onDialogShow(adventCalendarPopup, 'adventCalendarPopup');
 };
