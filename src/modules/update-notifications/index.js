9d36357ae8225e7137d9cbe15aa26e909f4767e3
Brad Parbs <brad@bradparbs.com>
2023-12-11 18:40:25 -0600

    Show update notifications for last minor version when the current version is a patch version

--- src/modules/update-notifications/index.js
+++ src/modules/update-notifications/index.js
@@ -28,37 +28,59 @@ const getReadUpdateNotificationTime = (version) => {
 
 /**
  * Show a notification to the user that MH Improved has been updated.
+ *
+ * @param {MessengerUINotification} m       The MessengerUINotification object.
+ * @param {string}                  version The version of MH Improved.
+ * @param {boolean}                 isPatch If the update is a patch update.
  */
+const showUpdateNotification = (m, version, isPatch = false) => {
+  const link = `https://github.com/MHCommunity/mousehunt-improved/releases/tag/v${version}`;
+
+  const { date, hasRead } = getReadUpdateNotificationTime(version);
+
+  // Check to see if the date is older than 1 week and if so, don't show the notification.
+  const threeWeeksAgo = new Date();
+  threeWeeksAgo.setDate(threeWeeksAgo.getDate() - 21);
+
+  if (new Date(date) < threeWeeksAgo) {
+    return;
+  }
+
+  m.addMessage({
+    messageId: `mhui-update-notification-${version}`,
+    messageType: 'news',
+    messageData: {
+      text: `<img src="https://i.mouse.rip/mh-improved/icon-64.png" class="item" alt="MouseHunt Improved icon">MouseHunt Improved has been updated to v${version}! <a href="${link}">${isPatch ? 'View the changelog' : 'Read the release notes'}</a>.`,
+      href: link,
+      cssClass: `mhui-notification ${isPatch ? 'mhui-notification-minor' : 'mhui-notification-major'}`,
+    },
+    messageDate: date,
+    isNew: ! hasRead,
+  });
+};
+
 /**
  * Initialize the module.
  */
 const init = () => {
   addUIStyles(styles);
 
-  const link = `https://github.com/MHCommunity/mousehunt-improved/releases/tag/v${mhImprovedVersion}`;
+  const version = mhImprovedVersion.split('.');
+  const m = new MessengerUINotification();
 
-  const { date, hasRead } = getReadUpdateNotificationTime(mhImprovedVersion);
-
-  // Check to see if the date is older than 1 week and if so, don't show the notification.
-  const oneWeekAgo = new Date();
-  oneWeekAgo.setDate(oneWeekAgo.getDate() - 7);
-
-  if (new Date(date) < oneWeekAgo) {
+  // If our version is somehow not 3 parts, just use the full version.
+  if (version.length !== 3) {
+    showUpdateNotification(m, mhImprovedVersion);
     return;
   }
 
-  const m = new MessengerUINotification();
-  m.addMessage({
-    messageId: `mhui-update-notification-${mhImprovedVersion}`,
-    messageType: 'news',
-    messageData: {
-      text: `<img src="https://i.mouse.rip/mh-improved/icon-64.png" class="item" alt="MouseHunt Improved icon">MouseHunt Improved has been updated to v${mhImprovedVersion}! <a href="${link}">Read the release notes</a>.`,
-      href: link,
-      cssClass: 'mhui-notification',
-    },
-    messageDate: date,
-    isNew: ! hasRead,
-  });
+  // If it's a major update, show the notification.
+  if (version[2] === '0') {
+    showUpdateNotification(m, mhImprovedVersion);
+  } else {
+    showUpdateNotification(m, `${version[0]}.${version[1]}.0`);
+    showUpdateNotification(m, mhImprovedVersion, true);
+  }
 };
 
 export default {
