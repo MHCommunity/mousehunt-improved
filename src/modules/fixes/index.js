01b3f88f757956d537e071b7ae0a19a2dea87115
Brad Parbs <brad@bradparbs.com>
2023-12-18 19:25:34 -0600

    Fix passing parcel message item opening passing parcel collectible item view

--- src/modules/fixes/index.js
+++ src/modules/fixes/index.js
@@ -13,16 +13,16 @@ const fixPassingParcel = () => {
     return;
   }
 
-  const newMarkup = `<div class="inventoryPage-item full convertible " onclick="app.pages.InventoryPage.useItem(this); return false;" data-item-id="1281" data-item-type="passing_parcel_convertible" data-item-classification="convertible" data-name="Passing Parcel" data-display-order="0">
+  const newMarkup = `<div class="inventoryPage-item full convertible " onclick="app.pages.InventoryPage.useItem(this); return false;" data-item-id="1281" data-item-type="passing_parcel_message_item" data-item-classification="convertible" data-name="Passing Parcel" data-display-order="0">
 	<div class="inventoryPage-item-margin clear-block">
 		<div class="inventoryPage-item-name">
-      <a href="#" class="" onclick="hg.views.ItemView.show('passing_parcel_convertible'); return false;">
+      <a href="#" class="" onclick="hg.views.ItemView.show('passing_parcel_message_item'); return false;">
         <abbr title="Passing Parcel">Passing Parcel (collectible)</abbr>
       </a>
     </div>
-    <a href="#" class="inventoryPage-item-larryLexicon" onclick="hg.views.ItemView.show('passing_parcel_convertible'); return false;">?</a>
+    <a href="#" class="inventoryPage-item-larryLexicon" onclick="hg.views.ItemView.show('passing_parcel_message_item'); return false;">?</a>
     <div class="inventoryPage-item-imageContainer">
-      <div class="itemImage"><a href="#" class="" onclick="hg.views.ItemView.show('passing_parcel_convertible'); return false;">
+      <div class="itemImage"><a href="#" class="" onclick="hg.views.ItemView.show('passing_parcel_message_item'); return false;">
         <img src="https://www.mousehuntgame.com/images/items/message_items/5591e5c34f081715aaca4e95e97a3379.jpg?cv=2"></a>
           <div class="quantity">${quantity.innerText}</div>
         </div>
