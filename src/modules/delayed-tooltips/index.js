a1943460b448fcfd63df9dfccacc6bae119fbc47
Brad Parbs <brad@bradparbs.com>
2024-01-12 00:54:44 -0600

    Dont delay tooltips while shift is held

--- src/modules/delayed-tooltips/index.js
+++ src/modules/delayed-tooltips/index.js
@@ -7,6 +7,20 @@ import styles from './styles.css';
  */
 const init = async () => {
   addStyles(styles);
+
+  // While the shift key is pressed, add 'no-delayed-tooltips' to the body, which will have the effect of disabling the delayed tooltips.
+  document.addEventListener('keydown', (e) => {
+    if (e.shiftKey) {
+      document.body.classList.add('no-delayed-tooltips');
+    }
+  });
+
+  // When the shift key is released, remove 'no-delayed-tooltips' from the body, which will have the effect of re-enabling the delayed tooltips.
+  document.addEventListener('keyup', (e) => {
+    if (! e.shiftKey) {
+      document.body.classList.remove('no-delayed-tooltips');
+    }
+  });
 };
 
 export default {
