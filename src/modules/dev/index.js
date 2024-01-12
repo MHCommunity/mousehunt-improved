5260d5b9e4e6255e78c90cf0be4dc9cd171b8321
Brad Parbs <brad@bradparbs.com>
2024-01-12 00:57:47 -0600

    Clean up dev debugging

--- src/modules/dev/index.js
+++ src/modules/dev/index.js
@@ -8,6 +8,28 @@ const isDebugging = (flag) => {
   return Utils.getFlag(flag);
 };
 
+const debug = (message, ...args) => {
+  const textMessages = [];
+  const objectMessages = [];
+
+  for (const arg of args) {
+    if (typeof arg === 'string') {
+      textMessages.push(arg);
+    } else {
+      objectMessages.push(arg);
+    }
+  }
+
+  // eslint-disable-next-line no-console
+  console.log(
+    `%cMH Improved:%c ${message}`,
+    'color: #ff3434; font-weight: 900; font-size: 1.1em',
+    'color: inherit; font-weight: inherit; font-size: inherit',
+    ...textMessages,
+    ...objectMessages
+  );
+};
+
 const init = () => {
   // Add all the stuff from Utils to be accessible in the console as 'app.mhutils'
   window.app = window.app || {};
@@ -17,69 +39,68 @@ const init = () => {
   if (isDebugging('debug-dialog')) {
     let currentDialog = null;
     Utils.onDialogHide(() => {
-      Utils.devDebug(`Dialog hidden: ${currentDialog}`);
+      debug(`Dialog hidden: ${currentDialog}`);
     });
 
     Utils.onDialogShow(() => {
       currentDialog = Utils.getCurrentDialog();
-      Utils.devDebug(`Dialog shown: ${currentDialog}`);
+      debug(`Dialog shown: ${currentDialog}`);
     });
   }
 
   if (isDebugging('debug-navigation')) {
     Utils.onNavigation(() => {
-      Utils.devDebug(`Navigated to ${Utils.getCurrentPage()} / ${Utils.getCurrentTab()} / ${Utils.getCurrentSubtab()}`);
+      debug('onNavigation', {
+        page: Utils.getCurrentPage(),
+        tab: Utils.getCurrentTab(),
+        subtab: Utils.getCurrentSubtab(),
+      });
     });
 
     Utils.onTravel(null, () => {
-      Utils.devDebug(`Traveled to ${Utils.getCurrentLocation()}`);
+      debug('onTravel', Utils.getCurrentLocation());
     });
   }
 
   if (isDebugging('debug-request')) {
     Utils.onRequest((response) => {
-      Utils.devDebug('Request made:');
-      console.log(response); // eslint-disable-line no-console
-      Utils.devDebug('---');
+      debug('onRequest', response);
     });
   }
 
   if (isDebugging('debug-events')) {
     const events = [
       'ajax_response',
+      'app_init',
+      'camp_page_arm_item',
+      'camp_page_toggle_blueprint',
+      'camp_page_update_item_array',
       'camp_quest_hud_view_initialize',
       'checkout_cart_update',
+      'info_arrow_hide',
       'info_arrow_show',
       'js_dialog_hide',
       'js_dialog_show',
+      'set_inset_tab',
+      'set_page',
+      'set_tab',
+      'spring_hunt_claim_hidden_egg',
+      'tournament_status_change',
       'travel_complete',
       'treasure_map_update_favourite_friends',
       'treasure_map_update_sent_requests',
       'treasure_map_update',
-      'user_relationship',
-      'user_inventory_update',
-      'user_recipe_update',
-      'user_trap_update',
-      'app_init',
-      'camp_page_arm_item',
-      'spring_hunt_claim_hidden_egg',
-      'info_arrow_hide',
-      'user_inventory_use_convertible',
-      'set_inset_tab',
-      'set_page',
-      'set_tab',
-      'info_arrow_show',
-      'camp_page_toggle_blueprint',
-      'camp_page_update_item_array',
       'user_interaction_update',
-      'tournament_status_change',
+      'user_inventory_update',
+      'user_inventory_use_convertible',
+      'user_recipe_update',
+      'user_relationship',
+      'user_trap_update',
     ];
 
     events.forEach((event) => {
       Utils.onEvent(event, (...data) => {
-        Utils.devDebug(`Event fired: "${event}" with data:`);
-        console.log(...data); // eslint-disable-line no-console
-        Utils.devDebug('---');
+        debug(`onEvent: ${event}`, data);
       });
     });
   }
