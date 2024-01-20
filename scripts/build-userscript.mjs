8e28d02b409ba7b023ca1dad4636ed87af9171b2
Brad Parbs <brad@bradparbs.com>
2024-01-20 15:08:31 -0600

    Update userscript build for sentry, add to ignore

--- scripts/build-userscript.mjs
+++ scripts/build-userscript.mjs
@@ -1,3 +1,5 @@
+import { sentryEsbuildPlugin } from '@sentry/esbuild-plugin';
+
 import { CSSMinifyTextPlugin, sharedBuildOptions } from './shared.mjs';
 
 import * as esbuild from 'esbuild';
@@ -8,12 +10,29 @@ const header = fs.readFileSync(
   path.join(process.cwd(), 'src/userscript-header.js'), 'utf8')
   .replace('process.env.VERSION', process.env.npm_package_version);
 
+const plugins = [
+  CSSMinifyTextPlugin,
+];
+
+// If we're building for production, add the Sentry plugin.
+if ('production' === process.env.NODE_ENV) {
+  plugins.push(sentryEsbuildPlugin({
+    authToken: process.env.SENTRY_AUTH_TOKEN,
+    org: 'brad-parbs',
+    project: 'mh-improved',
+    telemetry: false,
+    release: {
+      name: `mousehunt-improved-userscript@${process.env.npm_package_version}`,
+    }
+  }));
+}
+
 await esbuild.build({
   ...sharedBuildOptions,
-  plugins: [CSSMinifyTextPlugin],
+  plugins,
   outfile: 'dist/mousehunt-improved.user.js',
   minify: false,
-  sourcemap: false,
+  sourcemap: true,
   banner: {
     js: [
       header,
