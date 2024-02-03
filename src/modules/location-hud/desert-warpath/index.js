e7bfdb91a341a6a1b2ded4da7cdd9c988415c210
Brad Parbs <brad@bradparbs.com>
2024-02-02 18:16:45 -0600

    Add minigame to fiery warpath hud

--- src/modules/location-hud/desert-warpath/index.js
+++ src/modules/location-hud/desert-warpath/index.js
@@ -2,9 +2,78 @@ import { addHudStyles } from '@utils';
 
 import styles from './styles.css';
 
+let engaged = false;
+const addMissiles = () => {
+  const container = document.querySelector('.warpathHUD-waveContainer');
+  if (! container) {
+    return;
+  }
+
+  const trigger = document.querySelector('.warpathHUD-streakContainer');
+  if (! trigger) {
+    return;
+  }
+
+  trigger.addEventListener('click', () => {
+    engaged = ! engaged;
+
+    if (engaged) {
+      container.classList.add('warpathHUD-engaged');
+    } else {
+      container.classList.remove('warpathHUD-engaged');
+    }
+  });
+
+  const launchMissle = (event) => {
+    if (! engaged) {
+      return;
+    }
+
+    // Get the x coordinate of the mouse click for the element.
+    let x = event.clientX ? event.clientX - container.getBoundingClientRect().left : Math.floor(Math.random() * container.getBoundingClientRect().width);
+
+    // Randomize the x coordinate a bit.
+    x += Math.floor(Math.random() * 50) - 25;
+
+    hg.views.HeadsUpDisplayDesertWarpathView.spawnMissile(x, 1);
+
+    let missile;
+    setTimeout(() => {
+      missile = container.querySelectorAll('.warpathHUD-missile[style*="left: ' + x + 'px"]');
+      if (! missile || missile.length === 0) {
+        return;
+      }
+
+      missile.forEach((m) => {
+        setTimeout(() => {
+          m.classList.add('mh-ui-fade');
+        }, 50);
+
+        setTimeout(() => {
+          m.classList.add('mh-ui-fade-out');
+        }, 100);
+
+        setTimeout(() => {
+          m.remove();
+        }, 500);
+      });
+    }, 700);
+  };
+
+  container.addEventListener('click', launchMissle);
+  document.addEventListener('keydown', (event) => {
+    if (event.code === 'Space') {
+      event.preventDefault();
+      launchMissle(event);
+    }
+  });
+};
+
 /**
  * Initialize the module.
  */
 export default async () => {
   addHudStyles(styles);
+
+  addMissiles();
 };
