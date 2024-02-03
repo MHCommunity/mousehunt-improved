160972e2deddc5f7eaf104bc20743eb3eed7b52c
Brad Parbs <brad@bradparbs.com>
2024-02-03 15:39:09 -0600

    Add database utility functions

---
+++ src/utils/db.js
@@ -0,0 +1,126 @@
+const database = async (databaseName) => {
+  const request = window.indexedDB.open('mh-improved', 1);
+
+  request.onupgradeneeded = (event) => {
+    const db = event.target.result;
+    const objectStore = db.createObjectStore(databaseName, { keyPath: 'id' });
+    objectStore.createIndex('id', 'id', { unique: true });
+  };
+
+  return new Promise((resolve, reject) => {
+    request.onsuccess = () => {
+      resolve(request.result);
+    };
+
+    request.onerror = () => {
+      reject(request.error);
+    };
+  });
+};
+
+const dbGet = async (databaseName, id) => {
+  const db = await database(databaseName);
+
+  const transaction = db.transaction(databaseName, 'readonly');
+
+  transaction.onerror = (event) => {
+    throw new Error(event.target.error);
+  };
+
+  const objectStore = transaction.objectStore(databaseName);
+  const request = objectStore.get(id);
+
+  return new Promise((resolve, reject) => {
+    request.onsuccess = () => {
+      resolve(request.result);
+    };
+
+    request.onerror = () => {
+      reject(request.error);
+    };
+  });
+};
+
+const dbSet = async (databaseName, data) => {
+  const db = await database(databaseName);
+
+  const transaction = db.transaction(databaseName, 'readwrite');
+  const objectStore = transaction.objectStore(databaseName);
+
+  const request = objectStore.add(data);
+
+  return new Promise((resolve, reject) => {
+    request.onsuccess = () => {
+      resolve(request.result);
+    };
+
+    request.onerror = () => {
+      reject(request.error);
+    };
+  });
+};
+
+const dbUpdate = async (databaseName, data) => {
+  const db = await database(databaseName);
+
+  const transaction = db.transaction(databaseName, 'readwrite');
+  const objectStore = transaction.objectStore(databaseName);
+
+  const request = objectStore.put(data);
+
+  return new Promise((resolve, reject) => {
+    request.onsuccess = () => {
+      resolve(request.result);
+    };
+
+    request.onerror = () => {
+      reject(request.error);
+    };
+  });
+};
+
+const dbDelete = async (databaseName, id) => {
+  const db = await database(databaseName);
+
+  const transaction = db.transaction(databaseName, 'readwrite');
+  const objectStore = transaction.objectStore(databaseName);
+
+  const request = objectStore.delete(id);
+
+  return new Promise((resolve, reject) => {
+    request.onsuccess = () => {
+      resolve(request.result);
+    };
+
+    request.onerror = () => {
+      reject(request.error);
+    };
+  });
+};
+
+const dbGetAll = async (databaseName) => {
+  const db = await database(databaseName);
+
+  const transaction = db.transaction(databaseName, 'readonly');
+  const objectStore = transaction.objectStore(databaseName);
+
+  const request = objectStore.getAll();
+
+  return new Promise((resolve, reject) => {
+    request.onsuccess = () => {
+      resolve(request.result);
+    };
+
+    request.onerror = () => {
+      reject(request.error);
+    };
+  });
+};
+
+export {
+  dbGet,
+  dbSet,
+  dbUpdate,
+  dbDelete,
+  dbGetAll,
+};
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,4 @@
+export * from './db';
 export * from './data';
 export * from './debug';
 export * from './draggable';
