e1635b72dc8f07f647b77b386b547797c942b988
Brad Parbs <brad@bradparbs.com>
2024-02-03 12:45:25 -0600

    Updaye marketplace search settings text

--- src/modules/better-marketplace/settings/index.js
+++ src/modules/better-marketplace/settings/index.js
@@ -8,7 +8,7 @@ import { addMhuiSetting } from '@utils';
 export default function (module) {
   addMhuiSetting(
     'better-marketplace-search-all',
-    'Default to showing all items in the marketplace search.',
+    'Show all items in search',
     false,
     'If disabled, then useless items will be hidden from the search dropdown by default.',
     module
