29318f646e22b4883afe975781c9a83a28f7d195
Brad Parbs <brad@bradparbs.com>
2024-02-03 12:46:07 -0600

    Add userscript styles for rift bristle woods HUD

--- src/modules/location-hud/rift-bristle-woods/index.js
+++ src/modules/location-hud/rift-bristle-woods/index.js
@@ -1,10 +1,28 @@
-import { addHudStyles } from '@utils';
+import { addHudStyles, addStyles } from '@utils';
 
 import styles from './styles.css';
+
+const maybeAddUserscriptStyles = () => {
+  const check = document.querySelector('.riftBristleWoodsHUD .item_container .item-Btn');
+  if (! check) {
+    return;
+  }
+
+  const userscriptStyles = [
+    '.riftBristleWoodsHUD .item_container, .riftBristleWoodsHUD .charm_container { margin-top: 15px; }',
+    '.riftBristleWoodsHUD { margin-bottom: 30px; }',
+  ].join('\n');
+
+  addStyles(userscriptStyles, 'riftBristleWoodsHUD', 'mh-improved-rift-bristle-woods-userscript-styles');
+};
 
 /**
  * Initialize the module.
  */
 export default () => {
   addHudStyles(styles);
+
+  setTimeout(() => {
+    maybeAddUserscriptStyles();
+  }, 1000);
 };
