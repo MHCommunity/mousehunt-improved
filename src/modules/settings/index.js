a45a67a52d10f6491d02f1bd099358fef3a209c9
Brad Parbs <brad@bradparbs.com>
2024-02-18 10:59:18 -0600

    Update settings styles

--- src/modules/settings/index.js
+++ src/modules/settings/index.js
@@ -160,10 +160,6 @@ const modifySettingsPage = () => {
   settingsPage.forEach((setting) => {
     // Append an svg to toggle the class
     const toggle = makeElement('div', 'mhui-setting-toggle');
-    toggle.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-      <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
-    </svg>`;
-
     const titleText = setting.querySelector('.PagePreferences__titleText');
     if (titleText.childNodes.length > 1) {
       titleText.insertBefore(toggle, titleText.childNodes[1]);
