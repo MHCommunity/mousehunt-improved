ecef45500bd3e0f3e8b38474208f05e1a6ebc0b5
Brad Parbs <brad@bradparbs.com>
2024-02-21 19:12:27 -0600

    Split all userscript styling in Better UI to seperate files, toggleable with flags

--- src/modules/better-ui/index.js
+++ src/modules/better-ui/index.js
@@ -3,18 +3,43 @@ import { addStyles, getFlag, onNavigation } from '@utils';
 import friends from './friends';
 import hud from './hud';
 
-import profilePlusFixStyles from './profile-plus-fixes.css';
+import favoriteSetupsStyles from './userscript-styles/favorite-setups.css';
+import journalHistorianStyles from './userscript-styles/journal-historian.css';
+import lgsReminderStyles from './userscript-styles/lgs-reminder.css';
+import mhctStyles from './userscript-styles/mhct.css';
+import profilePlusStyles from './userscript-styles/profile-plus.css';
+import tsituLocationCatchStatsStyles from './userscript-styles/tsitu-location-catch-stats.css';
+import tsituQolStyles from './userscript-styles/tsitu-qol.css';
+import tsituSupplySearchStyles from './userscript-styles/tsitu-supply-search.css';
 
 import * as imported from './styles/*.css'; // eslint-disable-line import/no-unresolved
 const styles = imported;
 
-const addProfilePlusFixes = () => {
-  const hasProfilePlus = document.querySelector('#copyCrownsButton');
-  if (! hasProfilePlus) {
-    return;
-  }
+const addUserscriptStyles = () => {
+  const userscriptStyles = [
+    { id: 'better-ui-no-profile-plus-styles', styles: profilePlusStyles },
+    { id: 'better-ui-no-favorite-setups-styles', styles: favoriteSetupsStyles },
+    { id: 'better-ui-no-journal-historian-styles', styles: journalHistorianStyles },
+    { id: 'better-ui-no-lgs-reminder-styles', styles: lgsReminderStyles },
+    { id: 'better-ui-no-mhct-styles', styles: mhctStyles },
+    { id: 'better-ui-no-tsitu-location-catch-stats-styles', styles: tsituLocationCatchStatsStyles },
+    { id: 'better-ui-no-tsitu-qol-styles', styles: tsituQolStyles },
+    { id: 'better-ui-no-tsitu-supply-search-styles', styles: tsituSupplySearchStyles },
+  ];
 
-  addStyles(profilePlusFixStyles, 'better-ui-profile-plus-fixes');
+  userscriptStyles.forEach(({ id, userscriptStyle }) => {
+    if (! getFlag(id, false)) {
+      addStyles(userscriptStyle, id);
+    }
+  });
+
+  if (! getFlag('better-ui-no-profile-plus-styles', getFlag('no-better-ui-profile-plus', false))) {
+    onNavigation(() => {
+      setTimeout(profilePlusStyles, 1000);
+    }, {
+      page: 'hunterprofile',
+    });
+  }
 };
 
 /**
@@ -23,16 +48,9 @@ const addProfilePlusFixes = () => {
 const init = async () => {
   addStyles(styles, 'better-ui');
 
+  addUserscriptStyles();
   friends();
   hud();
-
-  if (! getFlag('no-better-ui-profile-plus', false)) {
-    onNavigation(() => {
-      setTimeout(addProfilePlusFixes, 1000);
-    }, {
-      page: 'hunterprofile',
-    });
-  }
 };
 
 export default {
