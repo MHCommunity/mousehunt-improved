82880228db74d04fec8f0371d48c9530d7d7f934
Brad Parbs <brad@bradparbs.com>
2024-02-21 19:12:57 -0600

    Also show convertibles as loot list for Better Journal

--- src/modules/better-journal/journal-list/index.js
+++ src/modules/better-journal/journal-list/index.js
@@ -1,6 +1,71 @@
 import { addStyles, onEvent } from '@utils';
 import styles from './styles.css';
 
+const makeListItems = (itemList) => {
+  const list = document.createElement('ul');
+  list.classList.add('better-journal-list');
+
+  if (0 === itemList.length) {
+    return list;
+  }
+
+  itemList.forEach((item) => {
+    const listItem = document.createElement('li');
+    listItem.innerHTML = item.trim();
+    list.append(listItem);
+  });
+
+  return list;
+};
+
+const splitText = (text) => {
+  const splitItems = text.split(/, | and /);
+  return splitItems.map((item) => item.trim()).filter(Boolean);
+};
+
+const getItemsFromText = (type, text) => {
+  let items;
+  if ('loot' === type) {
+    items = text.innerHTML.split(' that dropped ');
+    if (items.length < 2) {
+      return {
+        list: [],
+        newText: text.innerHTML,
+      };
+    }
+
+    return {
+      items,
+      list: splitText(items[1]),
+      newText: `${items[0]} that dropped:`,
+    };
+  }
+
+  if ('convertible' === type) {
+    items = text.innerHTML.split('I received ');
+    if (items.length < 2) {
+      return {
+        list: [],
+        newText: text.innerHTML,
+      };
+    }
+
+    // if it's a convertible, we want to get the "from X gifts" part as well
+    const suffix = items[1].split(' from ');
+    if (suffix.length < 2) {
+      return {
+        list: splitText(items[1]),
+        newText: 'I received:',
+      };
+    }
+
+    return {
+      list: splitText(suffix[0]),
+      newText: `I opened ${suffix[1]} and received:`,
+    };
+  }
+};
+
 /**
  * Format a journal entry as a list.
  *
@@ -16,38 +81,36 @@ const formatAsList = async (args) => {
     return;
   }
 
+  const lootClassesToCheck = [
+    'bonuscatchsuccess',
+    'catchsuccess',
+    'catchsuccessloot',
+    'luckycatchsuccess',
+  ];
+
+  const convertibleClassesToCheck = [
+    'convertible_open',
+  ];
+
   const classes = new Set(entry.classList);
-  if (! (
-    classes.has('bonuscatchsuccess') ||
-    classes.has('catchsuccess') ||
-    classes.has('catchsuccessloot') ||
-    classes.has('luckycatchsuccess')
-  )) {
+
+  let type;
+
+  // Check if the entry is in either of the classes, and then call the function to format it as a list
+  if (lootClassesToCheck.some((c) => classes.has(c))) {
+    type = 'loot';
+  } else if (convertibleClassesToCheck.some((c) => classes.has(c))) {
+    type = 'convertible';
+  } else {
     return;
   }
 
   entry.setAttribute('data-better-journal-processed', 'true');
 
-  // split on ' dropped ' to get the items as the second element
-  const items = text.innerHTML.split(' that dropped ');
-  if (items.length < 2) {
-    return;
-  }
+  const { newText, list } = getItemsFromText(type, text);
 
-  const splitItems = items[1].split(/, | and /);
-  const itemList = splitItems.map((item) => item.trim()).filter(Boolean);
-
-  const list = document.createElement('ul');
-  list.classList.add('better-journal-list');
-
-  itemList.forEach((item) => {
-    const listItem = document.createElement('li');
-    listItem.innerHTML = item.trim();
-    list.append(listItem);
-  });
-
-  text.innerHTML = `${items[0]} that dropped:`;
-  text.append(list);
+  text.innerHTML = newText;
+  text.append(makeListItems(list));
 };
 
 /**
