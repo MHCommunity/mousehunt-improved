28223e700ddd85f934e84fa99e966d90f5d69203
Brad Parbs <brad@bradparbs.com>
2024-03-06 22:31:50 -0600

    Add limits

--- src/modules/location-huds/event-locations/birthday/index.js
+++ src/modules/location-huds/event-locations/birthday/index.js
@@ -1,7 +1,57 @@
-import { addHudStyles, addStyles } from '@utils';
+import { addHudStyles, addStyles, makeElement, onDialogShow } from '@utils';
 
 import styles from './styles.css';
 import stylesGlobal from './global.css';
+
+const changeColors = () => {
+  const popup = document.querySelector('.superBrieFactoryVendingMachinePopup');
+  if (! popup) {
+    return;
+  }
+
+  const can = makeElement('div', 'vending-machine-can');
+  const updateColor = () => {
+    const hue = Math.floor(Math.random() * 360);
+    can.style.filter = `hue-rotate(${hue}deg)`;
+  };
+
+  can.addEventListener('click', updateColor);
+  updateColor();
+  popup.append(can);
+
+  const hat = makeElement('div', 'vending-machine-hat');
+
+  // Get the hat element
+  let initialMouseX = 0;
+  let initialHatX = 0;
+
+  // Flag to check if dragging is happening
+  let isDragging = false;
+
+  // Add event listeners
+  hat.addEventListener('mousedown', (event) => {
+    isDragging = true;
+    initialMouseX = event.clientX;
+    initialHatX = Number.parseInt(window.getComputedStyle(hat).left, 10);
+  });
+
+  document.addEventListener('mousemove', (event) => {
+    if (isDragging) {
+      const deltaX = event.clientX - initialMouseX;
+      const newPosition = initialHatX + deltaX;
+
+      if (newPosition >= 25 && newPosition <= 415) {
+        hat.style.left = `${newPosition}px`;
+      }
+    }
+  });
+
+  document.addEventListener('mouseup', () => {
+    isDragging = false;
+  });
+
+  popup.append(hat);
+};
 
 /**
  * Always active.
@@ -15,6 +65,9 @@ const birthdayGlobal = async () => {
  */
 const birthdayLocation = async () => {
   addHudStyles(styles);
+  onDialogShow('superBrieFactoryVendingMachinePopup', () => {
+    setTimeout(changeColors, 500);
+  });
 };
 
 export {
