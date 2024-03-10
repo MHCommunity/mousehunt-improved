74e1b612be617020ae6a35e9bdb20714c0795fc5
Brad Parbs <brad@bradparbs.com>
2024-03-10 08:14:36 -0500

    remove onboarding

--- src/modules/update-notifications/index.js
+++ src/modules/update-notifications/index.js
@@ -36,34 +36,6 @@ const hasSeenBanner = () => {
 };
 
 /**
- * Check if the user has seen the onboarding step.
- *
- * @param {number} step The step to check.
- *
- * @return {boolean} True if the user has seen the onboarding step, false otherwise.
- */
-const hasSeenOnboarding = (step = 0) => {
-  if (getFlag('no-onboarding')) {
-    return true;
-  }
-
-  const savedStepNumber = getSetting('updates.onboarding', 0);
-  return savedStepNumber >= step;
-};
-
-/**
- * Save the onboarding step.
- *
- * @param {number} step The step to save.
- */
-const saveOnboardingStep = (step) => {
-  const savedStepNumber = getSetting('updates.onboarding', 0);
-  if (savedStepNumber < step) {
-    saveSetting('updates.onboarding', step);
-  }
-};
-
-/**
  * Add the update banner.
  *
  * @param {boolean} hasNewSettings Whether there are new settings.
@@ -131,101 +103,6 @@ const addBanner = (hasNewSettings = false) => {
 };
 
 /**
- * Register an onboarding step.
- *
- * @param {Object}   options                   The options for the onboarding step.
- * @param {string}   options.highlightSelector The selector to highlight.
- * @param {string}   options.classname         The class name to add to the onboarding step.
- * @param {string}   options.content           The content for the onboarding step.
- * @param {string}   options.button            The button text for the onboarding step.
- * @param {string}   options.direction         The direction for the onboarding step.
- * @param {Function} options.onShowCallback    The callback to run when the onboarding step is shown.
- * @param {Function} options.onCloseCallback   The callback to run when the onboarding step is closed.
- * @param {number}   options.step              The step number for the onboarding step.
- * @param {string}   options.page              The page to show the onboarding step on.
- * @param {string}   options.tab               The tab to show the onboarding step on.
- */
-const registerOnboardingStep = (options) => {
-  const {
-    highlightSelector,
-    classname,
-    content,
-    button,
-    direction,
-    onShowCallback,
-    onCloseCallback,
-    step,
-    page,
-    tab,
-  } = options;
-
-  if (hasSeenOnboarding(step)) {
-    return;
-  }
-
-  debuglog('update-notifications', `Registering onboarding step ${step}`);
-
-  if (page && page !== getCurrentPage()) {
-    return;
-  }
-
-  if (tab && tab !== getCurrentTab()) {
-    return;
-  }
-
-  hg.views.MessengerView.addMessage({
-    content: {
-      body: hg.views.OnboardingTutorialView().wrapInfoArrow(content, button || 'OK'),
-    },
-    highlight_dom: highlightSelector,
-    highlight_padding: 3,
-    css_class: `larryCircle mh-improved-onboarding ${classname || ''}`,
-    on_show_callback() {
-      hg.views.OnboardingTutorialView().showBouncyArrow(highlightSelector, direction);
-      if (onShowCallback) {
-        onShowCallback();
-      }
-    },
-    on_close_callback() {
-      hg.views.OnboardingTutorialView().hideBouncyArrow();
-
-      saveOnboardingStep(step);
-
-      if (onCloseCallback) {
-        onCloseCallback();
-      }
-    },
-    show_overlay: true,
-  });
-
-  hg.views.MessengerView.go();
-};
-
-/**
- * Do the onboarding.
- */
-const doOnboarding = () => {
-  registerOnboardingStep({
-    step: 1,
-    page: 'camp',
-    highlightSelector: '.mousehunt-improved-icon-menu',
-    classname: 'mh-improved-onboarding-icon',
-    content: 'Welcome to the new version of MouseHunt Improved! You can quickly get to the settings using this icon.',
-    direction: 'bottomLeft',
-  });
-
-  registerOnboardingStep({
-    step: 2,
-    page: 'preferences',
-    tab: 'mousehunt-improved-settings',
-    highlightSelector: '#mousehunt-improved-settings-better',
-    classname: 'mh-improved-onboarding-settings',
-    content: 'Here you can toggle features on and off, and customize them to your liking.',
-    direction: 'top',
-  });
-};
-
-/**
  * Initialize the module.
  */
 const init = async () => {
@@ -236,11 +113,6 @@ const init = async () => {
   }
 
   addBanner(true); // True if there are new settings, otherwise false.
-
-  // Delay the onboarding a bit so the banner has time to show.
-  onNavigation(() => {
-    setTimeout(doOnboarding, 1500);
-  });
 };
 
 export default {
