9b7d82485926185253763595fdd4ba82c54b9ea7
Brad Parbs <brad@bradparbs.com>
2024-03-23 03:34:57 -0500

    Update profile scoreboards

--- src/modules/experiments/modules/profile-scoreboard-search/index.js
+++ src/modules/experiments/modules/profile-scoreboard-search/index.js
@@ -1,4 +1,4 @@
-import { addStyles, getData, makeElement, onNavigation } from '@utils';
+import { addStyles, doRequest, getData, makeElement, onNavigation } from '@utils';
 
 import styles from './styles.css';
 
@@ -37,7 +37,7 @@ const main = async () => {
   scoreboardTab.setAttribute('data-tab', 'scoreboard');
   const scoreboardTabText = scoreboardTab.querySelector('span');
   if (scoreboardTabText) {
-    scoreboardTabText.textContent = 'Scoreboard';
+    scoreboardTabText.textContent = 'Scoreboards';
   }
 
   // Append it after the team tab.
@@ -46,8 +46,6 @@ const main = async () => {
   // Duplicate the team tab content and change the data-tab attribute to "scoreboard"
   const scoreboardTabContent = teamTabContent.cloneNode(true);
   scoreboardTabContent.setAttribute('data-tab', 'scoreboard');
-
-  const existingTeamTabContent = scoreboardTabContent.querySelector('.hunterInfoView-teamTab-content');
 
   const tabContent = makeElement('div', 'hunterInfoView-teamTab-content');
 
@@ -67,18 +65,60 @@ const main = async () => {
     scoreboardDropdown.append(option);
   }
 
+  tabContent.append(scoreboardDropdown);
+
+  const results = makeElement('div', 'mh-improved-scoreboard-results');
+  tabContent.append(results);
+
   scoreboardDropdown.addEventListener('change', async (e) => {
     if (e.target.value === 'placeholder' || ! friendName.getAttribute('data-text') || ! e.target.value) {
       return;
     }
 
-    window.location.href = `https://www.mousehuntgame.com/scoreboards.php?tab=main&scoreboard=${e.target.value}&search=${friendName.getAttribute('data-text')}`;
+    results.innerHTML = '<div class="mh-improved-scoreboard-loading"></div>';
+
+    const response = await doRequest('managers/ajax/pages/scoreboards.php', {
+      action: 'get_page',
+      category: 'main',
+      scoreboard: e.target.value,
+      page: 1,
+      weekly: 0,
+      friends_only: 0,
+      search: friendName.getAttribute('data-text'),
+    });
+
+    console.log(response);
+
+    const data = response?.scoreboard_page?.rows;
+    if (! data || ! data.length) {
+      results.innerHTML = '<div class="mh-improved-scoreboard-no-results">No results found.</div>';
+      return;
+    }
+
+    const score = data[0];
+
+    results.innerHTML = `<table class="scoreboardTableView">
+    <tr class="scoreboardTableView-row viewer highlight">
+      <td class="scoreboardTableView-row-rank scoreboardTableView-column ">
+        <div class="tournament-team-rank">
+          ${score.rank}
+        </div>
+      </td>
+      <td class="scoreboardTableView-row-name scoreboardTableView-column">
+        <a href="profile.php?snuid=${user.sn_user_id}" onclick="hg.utils.PageUtil.showHunterProfile('${user.sn_user_id}'); return false;">
+          <div class="scoreboardTableView-titleIcon" style="background-image: url(${user.title_icon});"></div>
+          ${score.name}
+        </a>
+      </td>
+      <td class="scoreboardTableView-row-score scoreboardTableView-column">
+        ${score.points_formatted}
+      </td>
+    </tr></table>`;
+
+    // window.location.href = `https://www.mousehuntgame.com/scoreboards.php?tab=main&scoreboard=${e.target.value}&search=${friendName.getAttribute('data-text')}`;
   });
 
-  tabContent.append(scoreboardDropdown);
-
-  existingTeamTabContent.replaceWith(tabContent);
-
+  scoreboardTabContent.replaceChildren(tabContent);
   // Append it after the team tab content.
   teamTabContent.after(scoreboardTabContent);
 
