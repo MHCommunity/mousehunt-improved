1e44305354d87ae0bbd69f8ac045f2c5d8921755
Brad Parbs <brad@bradparbs.com>
2024-03-25 19:42:54 -0500

    Updates styles for Windmill Location HUD, Adds easter egg when clicking on windmill speed for Windmill Location HUD

--- src/modules/location-huds/rift-burroughs/index.js
+++ src/modules/location-huds/rift-burroughs/index.js
@@ -37,7 +37,7 @@ const makeMiceList = (type, title, mice, currentType, appendTo) => {
     });
 
     const mouseImage = makeElement('img', 'mouse-type-mouse-image');
-    mouseImage.src = `https://www.mousehuntgame.com/images/mice/thumb/${miceNameImages[mouse]}.gif?cv=2`;
+    mouseImage.src = `https://www.mousehuntgame.com/images/mice/thumb/${miceNameImages[mouse]}`;
     mouseLink.append(mouseImage);
 
     makeElement('div', 'mouse-type-mouse-name', miceNameImages[mouse].name, mouseLink);
--- src/modules/location-huds/windmill/index.js
+++ src/modules/location-huds/windmill/index.js
@@ -2,9 +2,48 @@ import { addHudStyles } from '@utils';
 
 import styles from './styles.css';
 
+let start;
+const spinNeedle = () => {
+  const needle = document.querySelector('.windmillHud-needle');
+  if (! needle) {
+    return;
+  }
+
+  const speed = document.querySelector('.windmillHud-speed');
+  if (! speed) {
+    return;
+  }
+
+  start = needle.style.transform.match(/rotate\(([^)]+)\)/);
+  if (! start.length > 1) {
+    return;
+  }
+
+  start = Number.parseInt(start[1].replace('deg', ''), 10);
+
+  let timeout;
+  let spins = 1;
+  speed.addEventListener('click', () => {
+    needle.style.transition = 'transform 0.4s';
+    needle.style.transform = `rotate(${start + (360 * spins)}deg)`;
+
+    spins += 1;
+
+    clearTimeout(timeout);
+
+    timeout = setTimeout(() => {
+      needle.style.transition = 'none';
+      needle.style.transform = `rotate(${start}deg)`;
+
+      spins = 1;
+    }, spins * 400);
+  });
+};
+
 /**
  * Initialize the module.
  */
 export default async () => {
   addHudStyles(styles);
+  spinNeedle();
 };
