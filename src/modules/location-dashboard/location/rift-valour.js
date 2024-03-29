ac62b5d9a8d4fc4e30778c637e5c32d99322449d
Brad Parbs <brad@bradparbs.com>
2024-03-28 19:33:47 -0500

    Remove UU display in location dashboard - always displays

--- src/modules/location-dashboard/location/rift-valour.js
+++ src/modules/location-dashboard/location/rift-valour.js
@@ -19,12 +19,11 @@ export default (quests) => {
     speed: quests?.QuestRiftValour?.power_up_data?.long_stride?.current_level + 1 || 1,
     sync: quests?.QuestRiftValour?.power_up_data?.hunt_limit?.current_level + 1 || 1,
     siphon: quests?.QuestRiftValour?.power_up_data?.boss_extension?.current_level + 1 || 1,
-    umbra: quests?.QuestRiftValour?.augmentation_data?.tu?.is_locked === null ? 'Ultimate Umbra Run' : 'Normal Run',
   };
 
   let text = '';
 
   text = quest.floor === 0 ? 'Outside' : `Floor ${quest.floor} (${quest.floor_name}) · ${quest.hunts_remaining} hunts remaining`;
 
-  return `<div>${quest.umbra}</div> ${text} <div class="stats">Speed ${quest.speed} · Sync ${quest.sync} · Siphon ${quest.siphon}</div>`;
+  return `${text} <div class="stats">Speed ${quest.speed} · Sync ${quest.sync} · Siphon ${quest.siphon}</div>`;
 };
