a8f4d2062ac992f3c9e87c72050f02c153a8cfd2
Brad Parbs <brad@bradparbs.com>
2024-04-05 23:21:51 -0500

    Rewrite image upscaling

--- src/modules/image-upscaling/index.js
+++ src/modules/image-upscaling/index.js
@@ -14,280 +14,316 @@ import journalThemes from './journal-themes';
 import styles from './styles.css';
 import viewsStyles from './views.css';
 
-const stripUrl = (url) => {
-  return url
-    .replaceAll('//images', '/images')
-    .replace('https://www.mousehuntgame.com/images/', '')
-    .replaceAll('cv=1', '')
-    .replaceAll('cv=2', '')
-    .replaceAll('cv=3', '')
-    .replaceAll('asset_cache_version=1', '')
-    .replaceAll('asset_cache_version=2', '')
-    .replaceAll('?', '')
-    .replaceAll('&', '');
-};
-
-const getMappedUrl = (strippedUrl) => {
-  if (! strippedUrl) {
-    return;
+class ImageUpscaler {
+  constructor() {
+    this.mapping = [];
+    this.unupscaledImages = new Set();
+    this.upscaledImages = new Set();
+    this.lastCheck = { backgrounds: '', images: '' };
+    this.isUpscaling = false;
+    this.observerOptions = {
+      attributes: true,
+      attributeFilter: ['style'],
+      childList: true,
+      subtree: true,
+    };
+    this.observer = null;
+    this.debounceDelay = 250;
+    this.handleUpscalingImages = this.handleUpscalingImages.bind(this);
   }
 
-  const mappedUrl = mapping[strippedUrl];
-  if (! mappedUrl) {
-    return;
+  debounce(func, delay) {
+    let timeout;
+    return async (...args) => {
+      clearTimeout(timeout);
+      timeout = setTimeout(async () => await func(...args), delay);
+    };
   }
 
-  if (mappedUrl.includes('https://')) {
-    return mappedUrl;
+  stripUrl(url) {
+    const replacements = [
+      ['//images', '/images'],
+      ['https://www.mousehuntgame.com/images/', ''],
+      ['cv=1', ''],
+      ['cv=2', ''],
+      ['cv=3', ''],
+      ['cv=4', ''],
+      ['cv=5', ''],
+      ['asset_cache_version=1', ''],
+      ['asset_cache_version=2', ''],
+      ['asset_cache_version=3', ''],
+      ['asset_cache_version=4', ''],
+      ['asset_cache_version=5', ''],
+      ['?', ''],
+      ['&', ''],
+    ];
+
+    if (! url) {
+      return '';
+    }
+
+    return replacements.reduce((replacedUrl, [from, to]) => replacedUrl.replaceAll(from, to), url);
   }
 
-  if (mappedUrl) {
-    upscaledImages.push(mappedUrl);
+  getMappedUrl(strippedUrl) {
+    if (! strippedUrl) {
+      return;
+    }
+
+    const mappedUrl = this.mapping[strippedUrl];
+    if (! mappedUrl) {
+      return;
+    }
+
+    if (mappedUrl.includes('https://')) {
+      return mappedUrl;
+    }
+
+    if (mappedUrl) {
+      this.upscaledImages.add(mappedUrl);
+    }
+
+    return `https://www.mousehuntgame.com/images/${mappedUrl}`;
   }
 
-  return `https://www.mousehuntgame.com/images/${mappedUrl}`;
-};
+  shouldSkipUpdate(type, attribute, items) {
+    const itemHash = [...items]
+      .map((item) => item.getAttribute(attribute) || '')
+      .join(',');
 
-const shouldSkipUpdate = (type, attribute, items) => {
-  // hash the backgrounds to check if they are the same as the last check.
-  // if they are the same, skip the check.
-  const itemHash = [...items]
-    .map((item) => {
-      const itemAttribute = item.getAttribute(attribute);
-      if (! itemAttribute) {
-        return '';
+    const shouldSkip = this.lastCheck[type] === itemHash;
+    this.lastCheck[type] = itemHash;
+
+    return shouldSkip;
+  }
+
+  shouldSkipUrl(url) {
+    if (
+      this.unupscaledImages.has(url) || // Don't re-upscale images that have already been upscaled.
+      this.upscaledImages.has(url) ||
+      url.startsWith('https://www.gravatar.com') || // Skip some external images.
+      url.startsWith('https://graph.facebook.com') ||
+      url.startsWith('https://i.mouse.rip')
+    ) {
+      return true;
+    }
+
+    // Check if the image is in the list of images to skip.
+    // if the list has a path with a wildcard, check if the image path starts with the path.
+    // if the list has a path without a wildcard, check if the image path is equal to the path.
+    return pathsToSkip.some((path) => {
+      if (path.includes('*')) {
+        return url.startsWith(path.replace('*', ''));
       }
 
-      return itemAttribute;
-    })
-    .join(',');
-
-  if (lastCheck[type] && lastCheck[type] === itemHash) {
-    return true;
+      return url === path;
+    });
   }
-
-  lastCheck[type] = itemHash;
-
-  return false;
-};
-
-const shouldSkipUrl = (url) => {
-  if (
-    unupscaledImages.includes(url) || // Don't re-upscale images that have already been upscaled.
-    upscaledImages.includes(url) ||
-    url.startsWith('https://www.gravatar.com') || // Skip some external images.
-    url.startsWith('https://graph.facebook.com') ||
-    url.startsWith('https://i.mouse.rip')
-  ) {
-    return true;
-  }
-
-  // Check if the image is in the list of images to skip.
-  // if the list has a path with a wildcard, check if the image path starts with the path.
-  // if the list has a path without a wildcard, check if the image path is equal to the path.
-  const skip = pathsToSkip.some((path) => {
-    if (path.includes('*')) {
-      return url.startsWith(path.replace('*', ''));
-    }
-
-    return url === path;
-  });
-
-  return skip;
-};
-
-const upscaleImageElements = async () => {
-  const images = document.querySelectorAll('img');
-  if (! images) {
-    return;
-  }
-
-  if (shouldSkipUpdate('images', 'src', images)) {
-    return;
-  }
-
-  images.forEach((image) => {
-    const source = image.getAttribute('src');
-    if (! source) {
+  upscaleElement(element, attribute, urlExtractor) {
+    const attributeValue = element.getAttribute(attribute);
+    if (! attributeValue) {
       return;
     }
 
-    const url = stripUrl(source);
-
-    if (shouldSkipUrl(url)) {
+    const originalUrl = urlExtractor(attributeValue);
+    if (! originalUrl) {
       return;
     }
 
-    const mappedUrl = getMappedUrl(url);
-    if (mappedUrl && mappedUrl !== url) {
-      image.setAttribute('src', mappedUrl);
+    const url = this.stripUrl(originalUrl);
+
+    if (this.shouldSkipUrl(url)) {
+      return;
+    }
+
+    const mappedUrl = this.getMappedUrl(url);
+    if (mappedUrl && originalUrl !== mappedUrl) {
+    // Preload the image
+      const img = new Image();
+      img.onload = () => {
+      // Once the image is loaded, replace the URL in the original element
+        element.setAttribute(attribute, attributeValue.replace(originalUrl, mappedUrl));
+
+        // Set up a MutationObserver to prevent other code from changing the background-image
+        const observer = new MutationObserver((mutations) => {
+          mutations.forEach((mutation) => {
+            if (mutation.type === 'attributes' && mutation.attributeName === 'style') {
+              element.style.backgroundImage = `url(${mappedUrl})`;
+            }
+          });
+        });
+
+        observer.observe(element, { attributes: true });
+      };
+      img.src = mappedUrl;
     } else {
-      unupscaledImages.push(url);
+      this.unupscaledImages.add(url);
     }
-  });
-};
-
-const upscaleBackgroundImages = async () => {
-  const backgrounds = document.querySelectorAll('[style*="background-image"]');
-  if (! backgrounds) {
-    return;
   }
 
-  if (shouldSkipUpdate('backgrounds', 'style', backgrounds)) {
-    return;
-  }
-
-  backgrounds.forEach((background) => {
-    const style = background.getAttribute('style');
-    if (! style || ! style.includes('background-image')) {
+  upscaleImageElements() {
+    const images = document.querySelectorAll('img');
+    if (! images) {
       return;
     }
 
-    const urls = style.match(/url\((.*?)\)/);
-    if (! urls || ! urls[1]) {
+    if (this.shouldSkipUpdate('images', 'src', images)) {
       return;
     }
 
-    const url = stripUrl(urls[1].replaceAll(/["']+/g, ''));
+    images.forEach((image) => {
+      const originalUrl = image.getAttribute('src');
+      const strippedUrl = this.stripUrl(originalUrl);
 
-    if (shouldSkipUrl(url)) {
-      return;
-    }
-
-    const mappedUrl = getMappedUrl(url);
-    if (mappedUrl && mappedUrl !== url) {
-      background.setAttribute('style', style.replace(urls[1], mappedUrl));
-    } else {
-      unupscaledImages.push(url);
-    }
-  });
-};
-
-let debounceDelay = 0;
-
-const debounce = (func, delay) => {
-  debounceDelay = 250;
-  let timeout;
-  return async (...args) => {
-    clearTimeout(timeout);
-    timeout = setTimeout(async () => await func(...args), delay);
-  };
-};
-
-const upscaleImages = debounce(async () => {
-  if (isUpscaling) {
-    return;
-  }
-
-  isUpscaling = true;
-
-  observer.disconnect();
-
-  // Upscale the images.
-  await upscaleImageElements();
-  await upscaleBackgroundImages();
-
-  isUpscaling = false;
-
-  // Reconnect the observer
-  observer.observe(document, observerOptions);
-
-  return true;
-}, debounceDelay);
-
-let mapping = [];
-let observer;
-const unupscaledImages = [];
-const upscaledImages = [];
-const lastCheck = { backgrounds: '', images: '' };
-let isUpscaling = false;
-
-const observerOptions = {
-  attributes: true,
-  attributeFilter: ['style'],
-  childList: true,
-  subtree: true,
-};
-
-/**
- * Initialize the module.
- */
-const main = async () => {
-  if (observer) {
-    return;
-  }
-
-  observer = new MutationObserver(async (mutations) => {
-    const skipClasses = new Set([
-      'huntersHornView__timerState',
-      'mousehuntHud-gameInfo',
-      'campPage-daily-tomorrow-countDown',
-      'ticker',
-      'mousehuntHeaderView-menu-notification',
-      'mousehunt-improved-lgs-reminder-new',
-      'mousehunt-improved-lgs-reminder',
-      // Select2, search boxes on marketplace and friends list..
-      'select2-chosen',
-      'select2-offscreen',
-      'select2-container',
-      'select2-search',
-      'select2-drop',
-      'marketplaceView-header-searchContainer',
-      // Markethunt.
-      'highcharts-tracker',
-      'highcharts-grid',
-      'highcharts-axis',
-      'highcharts-axis-labels',
-    ]);
-
-    const skipIds = new Set([
-      'mh-improved-cre',
-      'mhhh_flast_message_div',
-    ]);
-
-    const skipElements = new Set([
-      'head',
-      'title',
-      'optgroup',
-      'option',
-    ]);
-
-    for (const mutation of mutations) {
-      if ((mutation.type === 'childList' || mutation.type === 'attributes') && (
-        (mutation.target.classList && [...mutation.target.classList].some((c) => skipClasses.has(c))) ||
-        (mutation.target.id && skipIds.has(mutation.target.id)) ||
-        (mutation.target.nodeName && skipElements.has(mutation.target.nodeName.toLowerCase()))
-      )) {
-        continue;
+      if (this.shouldSkipUrl(strippedUrl)) {
+        return;
       }
 
-      await upscaleImages(mutation.target);
+      const mappedUrl = this.getMappedUrl(strippedUrl);
+      if (mappedUrl && originalUrl !== mappedUrl) {
+        image.setAttribute('src', mappedUrl);
+      }
+    });
+  }
+
+  upscaleBackgroundImages() {
+    const backgrounds = document.querySelectorAll('[style*="background-image"]');
+    if (! backgrounds) {
+      return;
     }
-  });
 
-  observer.observe(document, observerOptions);
-};
+    if (this.shouldSkipUpdate('backgrounds', 'style', backgrounds)) {
+      return;
+    }
 
-const handleUpscalingImages = async () => {
-  if (! observer) {
-    await main();
+    backgrounds.forEach((background) => {
+      this.upscaleElement(background, 'style', (style) => {
+        const urls = style.match(/url\((.*?)\)/);
+        return urls && urls[1] ? urls[1].replaceAll(/["']+/g, '') : null;
+      });
+    });
   }
 
-  if (isUpscaling) {
-    return;
+  startObserver() {
+    if (this.observer) {
+      this.observer.disconnect();
+    }
+
+    this.observer = new MutationObserver(this.debounce(() => {
+      this.upscaleImageElements();
+      this.upscaleBackgroundImages();
+    }, this.debounceDelay));
+
+    this.observer.observe(document.body, this.observerOptions);
   }
 
-  await upscaleImages(document.querySelector('body'));
-};
+  async upscaleImages() {
+    if (this.isUpscaling) {
+      return;
+    }
+
+    this.isUpscaling = true;
+
+    await this.fetchMapping();
+
+    this.upscaleImageElements();
+    this.upscaleBackgroundImages();
+
+    this.startObserver();
+
+    this.isUpscaling = false;
+  }
+
+  async fetchMapping() {
+    this.mapping = await getData('upscaled-images');
+  }
+
+  async start() {
+    console.log('Starting image upscaling...'); // eslint-disable-line no-console
+    if (this.observer) {
+      return;
+    }
+
+    this.observer = new MutationObserver(async (mutations) => {
+      const skipClasses = new Set([
+        'huntersHornView__timerState',
+        'mousehuntHud-gameInfo',
+        'campPage-daily-tomorrow-countDown',
+        'ticker',
+        'mousehuntHeaderView-menu-notification',
+        'mousehunt-improved-lgs-reminder-new',
+        'mousehunt-improved-lgs-reminder',
+        // Select2, search boxes on marketplace and friends list..
+        'select2-chosen',
+        'select2-offscreen',
+        'select2-container',
+        'select2-search',
+        'select2-drop',
+        'marketplaceView-header-searchContainer',
+        // Markethunt.
+        'highcharts-tracker',
+        'highcharts-grid',
+        'highcharts-axis',
+        'highcharts-axis-labels',
+      ]);
+
+      const skipIds = new Set([
+        'mh-improved-cre',
+        'mhhh_flast_message_div',
+      ]);
+
+      const skipElements = new Set([
+        'head',
+        'title',
+        'optgroup',
+        'option',
+      ]);
+
+      for (const mutation of mutations) {
+        if ((mutation.type === 'childList' || mutation.type === 'attributes') && (
+          (mutation.target.classList && [...mutation.target.classList].some((c) => skipClasses.has(c))) ||
+          (mutation.target.id && skipIds.has(mutation.target.id)) ||
+          (mutation.target.nodeName && skipElements.has(mutation.target.nodeName.toLowerCase()))
+        )) {
+          continue;
+        }
+
+        try {
+          await this.upscaleImages(mutation.target);
+        } catch (error) {
+          console.error('Failed to upscale images:', error); // eslint-disable-line no-console
+        }
+      }
+    });
+
+    this.observer.observe(document, this.observerOptions);
+  }
+
+  async handleUpscalingImages() {
+    try {
+      if (! this.observer) {
+        await this.start();
+      }
+
+      if (! this.isUpscaling) {
+        await this.upscaleImages(document.querySelector('body'));
+      }
+    } catch (error) {
+      console.error('Failed to handle upscaling images:', error); // eslint-disable-line no-console
+    }
+  }
+}
 
 const init = async () => {
   addStyles([styles, viewsStyles], 'image-upscaling');
 
-  mapping = await getData('upscaled-images');
+  imageUpscaler = new ImageUpscaler();
+  imageUpscaler.handleUpscalingImages();
 
-  onEvent('mh-improved-init', handleUpscalingImages);
-  onDialogShow('all', handleUpscalingImages);
-  onRequest('*', handleUpscalingImages);
+  onRequest('*', imageUpscaler.handleUpscalingImages, true, [], true);
+
+  onEvent('mh-improved-init', imageUpscaler.handleUpscalingImages);
+  onDialogShow('all', imageUpscaler.handleUpscalingImages);
 
   if (! getFlag('no-image-upscaling-journal-themes')) {
     journalThemes();
