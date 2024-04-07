fdf5f111f7576cad0a208e1045fa988a095ec9f2
Brad Parbs <brad@bradparbs.com>
2024-04-07 00:38:54 -0500

    replace favicon after hunt

--- src/modules/experiments/modules/replace-favicon/index.js
+++ src/modules/experiments/modules/replace-favicon/index.js
@@ -1,6 +1,13 @@
-export default async () => {
+import { onTurn } from '@utils';
+
+const replace = () => {
   const favicon = document.querySelector('link[rel="SHORTCUT ICON"]'); // idk why it's all caps on the site, but it is.
   if (favicon) {
     favicon.href = 'https://www.mousehuntgame.com/images/ui/elements/golden_shield_page_shield_320.png';
   }
+};
+
+export default async () => {
+  replace();
+  onTurn(replace, 500);
 };
