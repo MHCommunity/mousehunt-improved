9c12c82a48926131a20100ee940e2971c7087ca0
Brad Parbs <brad@bradparbs.com>
2024-05-21 20:38:57 -0500

    Resort map scroll shop & add mhct links

--- src/modules/better-maps/modules/shops.js
+++ src/modules/better-maps/modules/shops.js
@@ -1,36 +1,92 @@
-import { onRequest } from '@utils';
+import { getCurrentLocation, makeElement, onRequest } from '@utils';
+
+const addMHCTLink = (action, scrollType) => {
+  const miceLink = makeElement('a', 'map-link', 'Mice List');
+  // TODO: convert the scrollType to the MHCT numeric ID.
+  miceLink.href = `https://mhct.win/mapper.php?item=${scrollType}`;
+  miceLink.target = '_blank';
+  miceLink.rel = 'noopener noreferrer';
+
+  action.appendChild(miceLink);
+  action.classList.add('has-link');
+};
 
 /**
  * Update the shops markup.
  */
 const updateShopsMarkup = async () => {
-  const shops = document.querySelectorAll('.treasureMapShopsView-shopItems .treasureMapPopup-shop');
-  if (! shops.length) {
+  const shopsNodeList = document.querySelectorAll('.treasureMapShopsView-shopItems .treasureMapPopup-shop');
+  if (!shopsNodeList.length) {
     return;
   }
 
+  let shops = Array.from(shopsNodeList);
+
+  shops.sort((a, b) => {
+    const aName = a.querySelector('.treasureMapPopup-shop-environment').textContent.trim().toLowerCase();
+    const bName = b.querySelector('.treasureMapPopup-shop-environment').textContent.trim().toLowerCase();
+    return aName.localeCompare(bName);
+  });
+
+  const currentLocation = getCurrentLocation();
+  shops = shops.sort((shop) => {
+    const environmentType = shop.querySelector('.treasureMapPopup-shop-environment').getAttribute('data-environment-type') || '';
+    return environmentType === currentLocation ? -1 : 0;
+  });
+
+  const shopContainer = document.querySelector('.treasureMapShopsView-shopItems');
+  while (shopContainer.firstChild) {
+    shopContainer.removeChild(shopContainer.firstChild);
+  }
+
   shops.forEach((shop) => {
-    const environment = shop.querySelector('.treasureMapPopup-shop-environment');
-    if (! environment) {
+    shopContainer.appendChild(shop);
+
+    const environmentEl = shop.querySelector('.treasureMapPopup-shop-environment');
+    if (! environmentEl) {
       return;
     }
 
-    const heading = environment.querySelector('.treasureMapView-block-content-heading');
+    const heading = environmentEl.querySelector('.treasureMapView-block-content-heading');
     if (! heading) {
       return;
     }
 
-    const container = environment.querySelector('.treasureMapPopup-shop-environmentItems');
+    const container = environmentEl.querySelector('.treasureMapPopup-shop-environmentItems');
     if (! container) {
       return;
     }
 
-    if (! environment.classList.contains('active')) {
+    if (! environmentEl.classList.contains('active')) {
       container.classList.add('hidden');
     }
 
     heading.addEventListener('click', () => {
       container.classList.toggle('hidden');
+    });
+
+    const scrolls = container.querySelectorAll('.treasureMapInventoryView-scrollCase');
+    if (! scrolls.length) {
+      return;
+    }
+
+    scrolls.forEach((scroll) => {
+      const action = scroll.querySelector('.treasureMapInventoryView-scrollCase-action');
+      if (! action) {
+        return;
+      }
+
+      const button = action.querySelector('.mousehuntActionButton');
+      if (! button) {
+        return;
+      }
+
+      const mapType = button.getAttribute('data-item-type');
+      if (! mapType) {
+        return;
+      }
+
+      addMHCTLink(action, mapType);
     });
   });
 };
