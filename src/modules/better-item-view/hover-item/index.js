56599910405a795d28c22090450313c4f5935228
Brad Parbs <brad@bradparbs.com>
2024-06-05 17:41:15 -0500

    fix issues with hover item

--- src/modules/better-item-view/hover-item/index.js
+++ src/modules/better-item-view/hover-item/index.js
@@ -4,69 +4,82 @@ import {
   getSetting,
   makeElement,
   onEvent,
-  onRequest,
-  onTurn
+  onRequest
 } from '@utils';
 
 import styles from './styles.css';
 
-/**
- * Fetch the item data.
- *
- * @param {string} itemId The item ID.
- *
- * @return {Promise} The item data.
- */
-const fetchItemData = async (itemId) => {
-  const itemDataRequest = await doRequest('managers/ajax/users/userInventory.php', {
-    action: 'get_items',
-    'item_types[]': itemId,
-  });
-
-  return itemDataRequest?.items?.[0];
-};
+let lastItem;
+let lastItemId;
 
 /**
- * Generate the markup for an item.
+ * Fetches the item data and fills the item data wrapper with the item data.
  *
- * @param {Object} item The item data.
- *
- * @return {HTMLElement} The item markup.
+ * @param {string} itemId Item ID to fetch data for.
  */
-const makeItemMarkup = (item) => {
-  const itemData = makeElement('div', 'item-data');
-
-  if (item.thumbnail) {
-    const itemImage = makeElement('img', 'item-image');
-    itemImage.src = item.thumbnail;
-    itemData.append(itemImage);
+const fetchAndFillItemData = async (itemId) => {
+  if (isLoading) {
+    return;
   }
 
-  const itemText = makeElement('div', 'item-text');
+  isLoading = true;
 
-  makeElement('div', 'item-name', item.name || '', itemText);
+  let item;
+  if (lastItemId === itemId) {
+    item = lastItem;
+  } else {
+    const itemDataRequest = await doRequest('managers/ajax/users/userInventory.php', {
+      action: 'get_items',
+      'item_types[]': itemId,
+    });
+
+    if (! itemDataRequest?.items?.length) {
+      return;
+    }
+
+    item = itemDataRequest?.items[0];
+    lastItem = item;
+    lastItemId = itemId;
+  }
+
+  const itemData = makeElement('div', 'item-data');
+
+  const itemImage = makeElement('img', 'item-image');
+  itemImage.src = item.thumbnail;
+  itemData.append(itemImage);
+
+  const itemText = makeElement('div', 'item-text');
+  makeElement('div', 'item-name', item.name, itemText);
 
   const quantity = makeElement('div', 'item-quanity');
   makeElement('span', '', 'You own: ', quantity);
-  makeElement('span', '', Number.parseInt(item.quantity || 0).toLocaleString(), quantity);
+  makeElement('span', '', item.quantity.toLocaleString(), quantity);
+
   itemText.append(quantity);
 
   const description = makeElement('div', 'item-description');
+  // grab the first sentence of the description
   const firstSentence = item.description.match(/[^!.?]+[!.?]/);
   makeElement('div', 'item-description-text', firstSentence ? firstSentence[0] : item.description, description);
+
   itemData.append(itemText);
 
-  return itemData;
+  itemDataWrapper.innerHTML = itemData.outerHTML;
+
+  isLoading = false;
 };
 
+let debugPopup = false;
+let itemDataWrapper;
+let isLoading = false;
+
 /**
- * Generate the markup for the loading state.
+ * Makes the markup for the item data wrapper and appends it to the body.
  *
- * @param {Event} e The event.
- *
- * @return {HTMLElement} The loading markup.
+ * @param {string} itemId Item ID to fetch data for.
+ * @param {Event}  e      Event object.
  */
-const makeLoadingMarkup = (e) => {
+const makeItemMarkup = async (itemId, e) => {
   if (itemDataWrapper) {
     itemDataWrapper.remove();
   }
@@ -74,6 +87,8 @@ const makeLoadingMarkup = (e) => {
   itemDataWrapper = makeElement('div', 'item-data-wrapper');
   itemDataWrapper.id = 'item-data-wrapper';
   itemDataWrapper.innerHTML = '<span class="item-data-wrapper-loading">Loading...</span>';
+
+  fetchAndFillItemData(itemId);
 
   document.body.append(itemDataWrapper);
   const rect = e.target.getBoundingClientRect();
@@ -88,96 +103,35 @@ const makeLoadingMarkup = (e) => {
   itemDataWrapper.style.top = `${tooltipTop}px`;
   itemDataWrapper.style.left = `${left - (itemDataWrapper.offsetWidth / 2) + (rect.width / 2)}px`;
 
-  return itemDataWrapper;
+  if (e.target && ! debugPopup) {
+    e.target.addEventListener('mouseleave', () => {
+      itemDataWrapper.remove();
+    });
+  }
 };
 
-/**
- * Get the item data.
- *
- * @param {string} itemType The item type.
- *
- * @return {Promise} The item data.
- */
-const getItemData = async (itemType) => {
-  let item;
-  if (cachedItemData[itemType]) {
-    item = cachedItemData[itemType];
-  } else {
-    item = await fetchItemData(itemType);
-    cachedItemData[itemType] = item;
-
-    setTimeout(() => {
-      // remove the cached item data after 1min
-      delete cachedItemData[itemType];
-    }, 60 * 1000);
-  }
-
-  if (! item) {
-    return;
-  }
-
-  return item;
-};
-
-let itemDataWrapper;
-let cachedItemData = {};
-
-/**
- * Do the hover mice stuff.
- */
 const main = () => {
-  const itemLinks = document.querySelectorAll('.journal .content .entry .journaltext a[href*="https://www.mousehuntgame.com/item.php?item_type="]');
+  const itemLinks = document.querySelectorAll('.journal .content .entry .journaltext a[onclick*="ItemView.show"]');
   if (! itemLinks) {
     return;
   }
 
   itemLinks.forEach((link) => {
-    const itemType = link.getAttribute('href').match(/item_type=(\w+)/)[1];
+    const itemType = link.getAttribute('onclick').match(/'([^']+)'/)[1];
     link.setAttribute('onclick', `hg.views.ItemView.show('${itemType}'); return false;`);
 
-    let timeoutId = null;
-    let isMouseOver = false;
-
-    link.addEventListener('mouseenter', async (e) => {
-      isMouseOver = true;
-
-      if (timeoutId) {
-        clearTimeout(timeoutId);
-      }
-
-      timeoutId = setTimeout(async () => {
-        if (! isMouseOver) {
-          return;
-        }
-
-        makeLoadingMarkup(e);
-        const itemData = await getItemData(itemType);
-        if (itemData && itemDataWrapper && isMouseOver) {
-          itemDataWrapper.innerHTML = '';
-          itemDataWrapper.append(makeItemMarkup(itemData));
-        }
-      }, 500);
+    link.addEventListener('mouseover', (e) => {
+      makeItemMarkup(itemType, e);
     });
 
-    link.addEventListener('mouseleave', () => {
-      isMouseOver = false;
-
-      if (timeoutId) {
-        clearTimeout(timeoutId);
-      }
-
-      if (itemDataWrapper) {
-        itemDataWrapper.remove();
-      }
+    link.addEventListener('mouseout', () => {
+      itemDataWrapper.remove();
     });
   });
 };
 
-/**
- * Attach the hover mice functionality.
- */
-const hoverMice = () => {
-  addStyles(styles, 'better-item-view-hover-mice');
+export default () => {
+  addStyles(styles, 'better-item-view-hover-item');
 
   debugPopup = getSetting('debug.hover-popups', false);
 
@@ -192,8 +146,4 @@ const hoverMice = () => {
       hoverItem.remove();
     }
   });
-
-  onTurn(() => cachedItemData = {});
 };
-
-export default hoverMice;
