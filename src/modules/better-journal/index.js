2af6f442040c8717a67183876764f02b4b86d512
Brad Parbs <brad@bradparbs.com>
2024-06-06 00:01:41 -0500

    Refactor better-journal module initialization

--- src/modules/better-journal/index.js
+++ src/modules/better-journal/index.js
@@ -28,41 +28,37 @@ const init = async () => {
     replacements: getSetting('better-journal.replacements', true),
     goldAndPoints: getSetting('better-journal.gold-and-points', true),
     itemColors: getSetting('better-journal.item-colors', true),
-    progressLog: getSetting('better-journal.progress-log', true),
     history: getSetting('better-journal.journal-history', true),
   };
 
-  if (enabled.styles) {
-    journalStyles();
-  }
+  const modules = [
+    { enabled: enabled.styles, load: journalStyles },
+    { enabled: enabled.list, load: () => {
+      journalList();
+      if (enabled.icons || enabled.iconsMinimal) {
+        addStyles(listAndIconsStyles, 'better-journal-list-and-icons');
+      }
+    } },
+    { enabled: enabled.icons, load: () => {
+      journalIcons();
+      journalIconsMinimal();
+    } },
+    { enabled: enabled.iconsMinimal, load: () => {
+      if (! enabled.icons) {
+        journalIconsMinimal();
+      }
+    } },
+    { enabled: enabled.replacements, load: journalReplacements },
+    { enabled: enabled.goldAndPoints, load: journalGoldAndPoints },
+    { enabled: enabled.itemColors, load: journalItemColors },
+    { enabled: enabled.history, load: journalHistory },
+  ];
 
-  if (enabled.list) {
-    journalList();
-    if (enabled.icons || enabled.iconsMinimal) {
-      addStyles(listAndIconsStyles, 'better-journal-list-and-icons');
+  for (const module of modules) {
+    if (module.enabled) {
+      module.load();
     }
   }
-
-  if (enabled.icons) {
-    journalIcons();
-    journalIconsMinimal();
-  } else if (enabled.iconsMinimal) {
-    journalIconsMinimal();
-  }
-
-  if (enabled.replacements) {
-    journalReplacements();
-  }
-
-  if (enabled.goldAndPoints) {
-    journalGoldAndPoints();
-  }
-
-  if (enabled.itemColors) {
-    journalItemColors();
-  }
-
-  journalHistory(enabled.history);
 };
 
 /**
