bfa458e41acb7e6b5dfee3fbbd6bd71dd8ec5710
Brad Parbs <brad@bradparbs.com>
2024-06-09 18:02:05 -0500

    Add tons more journal item colors

--- src/modules/better-journal/modules/journal-item-colors/index.js
+++ src/modules/better-journal/modules/journal-item-colors/index.js
@@ -1,6 +1,6 @@
 import { addStyles } from '@utils';
 
-import colors from './colors.json';
+import colors from '@data/journal-item-colors.json';
 
 /**
  * Make the styles for the colors.
@@ -8,13 +8,7 @@ import colors from './colors.json';
  * @return {string} The styles.
  */
 const makeStyles = () => {
-  let styles = '';
-
-  colors.forEach((color) => {
-    styles += `.journal .entry a[href="https://www.mousehuntgame.com/item.php?item_type=${color.item}"] { color: ${color.color}; }`;
-  });
-
-  return styles;
+  return Object.entries(colors).map(([id, icon]) => `#overlayPopup.hunting_summary .lootContainer a[href="https://www.mousehuntgame.com/item.php?item_type=${id}"], .journal .entry a[href="https://www.mousehuntgame.com/item.php?item_type=${id}"] { color: ${icon}; }`).join(' ');
 };
 
 /**
