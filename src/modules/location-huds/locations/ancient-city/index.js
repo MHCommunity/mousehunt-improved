80bb7e4c02e74798932b0565d12a6b75c7e6e3ab
Brad Parbs <brad@bradparbs.com>
2024-06-15 17:18:57 -0500

    Add boss name to Zokor HUD

--- src/modules/location-huds/locations/ancient-city/index.js
+++ src/modules/location-huds/locations/ancient-city/index.js
@@ -2,9 +2,40 @@ import { addHudStyles } from '@utils';
 
 import styles from './styles.css';
 
+const addBossName = () => {
+  const types = {
+    y: 'Paladin Weapon Master', // Fealty
+    h: 'Manaforge Smith', // Tech
+    s: 'Soul Binder', // Scholar
+    t: 'Molten Midas', // Treasury
+    f: '', // Farming
+  };
+
+  const type = user?.quests?.QuestAncientCity?.district_type;
+  if (! type || ! types[type]) {
+    return;
+  }
+
+  const hudText = document.querySelector('.ancientCityHUD-districtName');
+  if (! hudText) {
+    return;
+  }
+
+  if (hudText.getAttribute('data-boss-name')) {
+    return;
+  }
+
+  hudText.setAttribute('data-boss-name', true);
+  const bossText = ` - ${types[type]}`;
+  hudText.textContent = `${hudText.textContent}${bossText}`;
+  hudText.textContent = hudText.textContent.replaceAll(`${bossText}${bossText}`, bossText); // Prevent duplicate text.
+};
+
 /**
  * Initialize the module.
  */
 export default async () => {
   addHudStyles(styles);
+
+  addBossName();
 };
