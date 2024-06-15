1117b7bf890bff9722e89d9a287cf5062541e0a9
Brad Parbs <brad@bradparbs.com>
2024-06-15 17:55:15 -0500

    Add warning for oil charms in Zokor HUD

--- src/modules/location-huds/locations/ancient-city/index.js
+++ src/modules/location-huds/locations/ancient-city/index.js
@@ -1,4 +1,4 @@
-import { addHudStyles, makeElement } from '@utils';
+import { addHudStyles, makeElement, onRequest, showHornMessage } from '@utils';
 
 import styles from './styles.css';
 
@@ -36,6 +36,31 @@ const addBossName = () => {
   makeElement('div', 'ancientCityHUD-bossName', types[type], hudText);
 };
 
+const warnForOilCharms = () => {
+  // Lantern Oil Charm and Super Lantern Oil Charm.
+  if (2142 != user.trinket_item_id && 2652 != user.trinket_item_id) { // eslint-disable-line eqeqeq
+    return;
+  }
+
+  const isSuper = 2652 == user.trinket_item_id; // eslint-disable-line eqeqeq
+
+  showHornMessage({
+    title: 'Don\'t waste your charm!',
+    text: `You have a ${isSuper ? 'Super ' : ' '}Lantern Oil Charm equipped.`,
+    image: `https://www.mousehuntgame.com/images/items/trinkets/large/${isSuper ? 'e4411478eb95a955daa3a71b3de078fd' : 'e90252a8e76d6e5e75209db95acee4d4'}.png`,
+    dismiss: 40000,
+    button: 'Disarm',
+    type: 'unknown_error',
+    classname: 'oil-charm-warning',
+    action: () => {
+      if (hg?.utils?.TrapControl?.disarmTrinket && hg?.utils?.TrapControl?.go) {
+        hg.utils.TrapControl.disarmTrinket();
+        hg.utils.TrapControl.go();
+      }
+    },
+  });
+};
+
 /**
  * Initialize the module.
  */
@@ -43,4 +68,7 @@ export default async () => {
   addHudStyles(styles);
 
   addBossName();
+
+  warnForOilCharms();
+  onRequest('users/changetrap.php', warnForOilCharms);
 };
