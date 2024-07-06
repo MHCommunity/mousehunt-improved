a4262a469e663e09fe3a5218ff6690d4dca66b42
Brad Parbs <brad@bradparbs.com>
2024-07-06 18:46:06 -0500

    Update links

--- src/modules/emotes/index.js
+++ src/modules/emotes/index.js
@@ -51,6 +51,6 @@ export default {
   name: 'Emotes',
   type: 'feature',
   default: true,
-  description: 'Replaces Discord-style emotes on corkboards like :jerry: with the actual image in map and profile corkboard messages',
+  description: 'Replaces Discord-style emotes on corkboards like :jerry: with the actual image in map and profile corkboard messages. <a href="https://github.com/MHCommunity/mousehunt-improved/blob/main/docs/emotes.md" target="_blank" rel="noopener noreferrer">View the list of supported emotes</a>',
   load: init,
 };
