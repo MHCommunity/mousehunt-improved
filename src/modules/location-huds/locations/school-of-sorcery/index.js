77bd6003df5c88f23876b056341e4b86272ae00c
Brad Parbs <brad@bradparbs.com>
2024-09-13 20:28:03 -0500

    Add highlight when on highest course in School of Sorcery course

--- src/modules/location-huds/locations/school-of-sorcery/index.js
+++ src/modules/location-huds/locations/school-of-sorcery/index.js
@@ -1,8 +1,60 @@
-import { addHudStyles, getSetting } from '@utils';
+import { addHudStyles, getSetting, makeElement, onRequest, onTurn } from '@utils';
 
 import cleanChalkboard from './clean-chalkboard.css';
 import regionStyles from '../../shared/folklore-forest/styles.css';
 import styles from './styles.css';
+
+const highlightIfHighest = () => {
+  const transcripts = user?.quests?.QuestSchoolOfSorcery?.transcript_data;
+  const currentCourse = user?.quests?.QuestSchoolOfSorcery?.current_course?.course_type;
+  const currentScore = user?.quests?.QuestSchoolOfSorcery?.current_course?.course_level;
+
+  if (! transcripts || ! currentCourse || ! currentScore) {
+    return;
+  }
+
+  const courseTranscript = transcripts.find(({ course_type }) => course_type === currentCourse);
+  const highestScore = courseTranscript?.highest_level || 0;
+
+  if (! courseTranscript) {
+    return;
+  }
+
+  const title = document.querySelector('.schoolOfSorceryCourseView__courseName');
+  if (highestScore > currentScore) {
+    title.classList.remove('schoolOfSorceryCourseView__courseName--highest');
+
+    const existingHighestMarker = title.querySelector('.schoolOfSorceryCourseView__highestMarker');
+    if (existingHighestMarker) {
+      existingHighestMarker.remove();
+    }
+
+    return;
+  }
+
+  if (! title.classList.contains('schoolOfSorceryCourseView__courseName--highest')) {
+    title.classList.add('schoolOfSorceryCourseView__courseName--highest');
+  }
+
+  const existingHighestMarker = title.querySelector('.schoolOfSorceryCourseView__highestMarker');
+  if (existingHighestMarker) {
+    return;
+  }
+
+  const highestMarker = makeElement('img', 'schoolOfSorceryCourseView__highestMarker');
+  highestMarker.src = 'https://www.mousehuntgame.com/images/ui/camp/trap/star_favorite.png';
+  highestMarker.src = 'https://www.mousehuntgame.com/images/ui/hud/rift_valour/upgrade_arrow.png';
+  highestMarker.alt = '';
+  highestMarker.setAttribute('title', 'New record for your highest in this course!');
+  highestMarker.addEventListener('click', () => {
+    const transcriptButton = document.querySelector('.headsUpDisplaySchoolOfSorceryView__viewTranscriptButton');
+    if (transcriptButton) {
+      transcriptButton.click();
+    }
+  });
+
+  title.appendChild(highestMarker);
+};
 
 /**
  * Initialize the module.
@@ -13,4 +65,7 @@ export default async () => {
     styles,
     getSetting('location-huds.school-of-sorcery-clean-chalkboard', false) && cleanChalkboard,
   ]);
+
+  highlightIfHighest();
+  onRequest('*', highlightIfHighest);
 };
