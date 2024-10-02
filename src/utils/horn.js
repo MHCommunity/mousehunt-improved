0c849e9b4641ee5bab20d64e3c895ee2b80d7ebf
Brad Parbs <brad@bradparbs.com>
2024-10-01 22:42:01 -0500

    Update horn message styles and add custom colors

--- src/utils/horn.js
+++ src/utils/horn.js
@@ -3,7 +3,8 @@ import { makeElement } from './elements';
 /**
  * Show a message in the horn dialog.
  *
- * Type can be one of these: bait_empty unknown_error bait_disarmed recent_turn recent_linked_turn puzzle.
+ * Type can be: bait_empty unknown_error bait_disarmed recent_turn recent_linked_turn puzzle info error warning success.
+ * Available colors: blue, cyan, cyan-alt, green, green-alt, grey, orange, orange-alt, pink, purple, red, red-alt, yellow.
  *
  * @param {Object}   options               Options for the message.
  * @param {string}   options.title         Title of the message. Keep it under 50 characters.
@@ -11,7 +12,8 @@ import { makeElement } from './elements';
  * @param {string}   options.button        Text of the button.
  * @param {Function} options.action        Callback for the button.
  * @param {number}   options.dismiss       Time to dismiss the message.
- * @param {string}   options.type          Type of the message.
+ * @param {string}   options.type          Type of the message. Controls the type of message displayed.
+ * @param {string}   options.color         Color of the message.
  * @param {string}   options.classname     Classname of the message.
  * @param {string}   options.image         URL of the image to display.
  * @param {string}   options.imageLink     URL for the image link.
@@ -29,12 +31,26 @@ const showHornMessage = (options) => {
     button: options.button || 'OK',
     action: options.action || (() => {}),
     dismiss: options.dismiss || null,
-    type: options.type || 'recent_linked_turn',
+    type: options.type || (options.color ? 'bait_disarmed' : 'recent_linked_turn'),
+    color: options.color || false,
     classname: options.classname || '',
     image: options.image || null,
     imageLink: options.imageLink || null,
     imageCallback: options.imageCallback || null,
   };
+
+  if ('info' === settings.type) {
+    type = 'bait_disarmed';
+  } else if ('error' === settings.type) {
+    settings.type = 'bait_disarmed';
+    settings.color = 'red';
+  } else if ('warning' === settings.type) {
+    settings.type = 'bait_disarmed';
+    settings.color = 'yellow';
+  } else if ('success' === settings.type) {
+    type = 'bait_disarmed';
+    settings.color = 'green';
+  }
 
   // do the other effects
   const backdrop = document.querySelector('.huntersHornView__backdrop');
@@ -50,25 +66,38 @@ const showHornMessage = (options) => {
   const fragment = document.createDocumentFragment();
 
   const messageWrapper = makeElement('div', ['huntersHornView__message huntersHornView__message--active', settings.classname]);
-  const message = makeElement('div', ['huntersHornMessageView', `huntersHornMessageView--${settings.type}`]);
+  const message = makeElement('div', [
+    'huntersHornMessageView',
+    'huntersHornMessageView--shadow',
+    `huntersHornMessageView--${settings.type}`,
+    settings.color ? `huntersHornMessageView--color-${settings.color}` : ''
+  ]);
+
   makeElement('div', 'huntersHornMessageView__title', settings.title, message);
   const content = makeElement('div', 'huntersHornMessageView__content');
+
   if (settings.image) {
-    const imgWrapper = makeElement('div', 'huntersHornMessageView__friend');
-    const img = makeElement('a', 'huntersHornMessageView__friendProfilePic');
-    if (settings.imageLink) {
-      img.href = settings.imageLink;
-    } else if (settings.imageCallback) {
-      img.addEventListener('click', settings.imageCallback);
+    if ('recent_turn' === settings.type || 'recent_linked_turn' === settings.type) {
+      const imgWrapper = makeElement('div', 'huntersHornMessageView__friend');
+      const img = makeElement('a', 'huntersHornMessageView__friendProfilePic');
+      if (settings.imageLink) {
+        img.href = settings.imageLink;
+      } else if (settings.imageCallback) {
+        img.addEventListener('click', settings.imageCallback);
+      } else {
+        img.href = '#';
+      }
+
+      img.style.backgroundImage = `url(${settings.image})`;
+
+      imgWrapper.append(img);
+      content.append(imgWrapper);
     } else {
-      img.href = '#';
+      content.classList.add('huntersHornMessageView__content--image');
+      content.setAttribute('style', `--bg-image: url(${settings.image})`);
     }
-
-    img.style.backgroundImage = `url(${settings.image})`;
-
-    imgWrapper.append(img);
-    content.append(imgWrapper);
   }
+
   makeElement('div', 'huntersHornMessageView__text', settings.text, content);
   const buttonSpacer = makeElement('div', 'huntersHornMessageView__buttonSpacer');
   const button = makeElement('button', 'huntersHornMessageView__action');
