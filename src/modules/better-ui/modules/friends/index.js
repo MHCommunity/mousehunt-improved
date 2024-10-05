134817b80feeb1244345e29276859c3a916bb833
Brad Parbs <brad@bradparbs.com>
2024-10-04 19:10:40 -0500

    Add functionality to toggle environments in camp page friends list

--- src/modules/better-ui/modules/friends/index.js
+++ src/modules/better-ui/modules/friends/index.js
@@ -1,4 +1,11 @@
-import { getCurrentPage, onNavigation } from '@utils';
+import {
+  getCurrentPage,
+  getSetting,
+  onNavigation,
+  onRequest,
+  saveSetting,
+  waitForElement
+} from '@utils';
 
 /**
  * Reorder the blocks on the friends page.
@@ -46,6 +53,73 @@ const autofocusIdSearch = () => {
   input.focus();
 };
 
+const addCampFriendsListener = () => {
+  const button = document.querySelector('.campPage-trap-friendContainer-toggleFriendsButton');
+  if (! button) {
+    return;
+  }
+
+  button.removeEventListener('click', updateCampFriends);
+  button.addEventListener('click', updateCampFriends);
+};
+
+const updateCampFriendsSetting = (environmentName, hidden) => {
+  const hiddenEnvironments = getSetting('better-ui.camp-hidden-friends', {});
+  if (hidden) {
+    hiddenEnvironments[environmentName] = true;
+  } else {
+    delete hiddenEnvironments[environmentName];
+  }
+
+  saveSetting('better-ui.camp-hidden-friends', hiddenEnvironments);
+};
+
+const toggleEnvironment = (event) => {
+  const title = event.target;
+  const friends = title.parentNode.querySelectorAll('a');
+
+  title.classList.toggle('friends-hidden');
+  friends.forEach((friend) => {
+    friend.classList.toggle('friends-hidden');
+  });
+
+  const name = title.getAttribute('data-environment-name');
+  const hidden = title.classList.contains('friends-hidden');
+
+  updateCampFriendsSetting(name, hidden);
+};
+
+const updateCampFriends = async () => {
+  await waitForElement('.campPage-trap-friendContainer-environment');
+  const environments = document.querySelectorAll('.campPage-trap-friendContainer-environment');
+  if (! environments) {
+    return;
+  }
+
+  const defaultHidden = getSetting('better-ui.camp-hidden-friends', {});
+
+  environments.forEach((environment) => {
+    const title = environment.querySelector('.campPage-trap-friendContainer-environment-title');
+    const friends = environment.querySelectorAll('a');
+    if (! title || ! friends) {
+      return;
+    }
+
+    const environmentName = title.textContent.trim().toLowerCase().replaceAll(' ', '-');
+    title.setAttribute('data-environment-name', environmentName);
+    title.setAttribute('data-number-of-friends', friends.length);
+    if (defaultHidden[environmentName]) {
+      title.classList.add('friends-hidden');
+      friends.forEach((friend) => {
+        friend.classList.add('friends-hidden');
+      });
+    }
+
+    title.removeEventListener('click', toggleEnvironment);
+    title.addEventListener('click', toggleEnvironment);
+  });
+};
+
 /**
  * Initialize the module.
  */
@@ -58,5 +132,10 @@ export default async () => {
     page: 'friends',
     tab: 'requests',
     subtab: 'community',
+  });
+
+  onRequest('users/getfriendsonline.php', updateCampFriends);
+  onNavigation(addCampFriendsListener, {
+    page: 'camp',
   });
 };
