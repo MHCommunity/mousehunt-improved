bed627669c60d37d36e4320b6892fc2d14f73f52
Brad Parbs <brad@bradparbs.com>
2025-04-23 14:32:12 -0500

    Add quick max reinforce to Draconic Depths HUD

--- src/modules/location-huds/locations/draconic-depths/index.js
+++ src/modules/location-huds/locations/draconic-depths/index.js
@@ -1,10 +1,103 @@
-import { addHudStyles } from '@utils';
+import {
+  addHudStyles,
+  makeElement,
+  onRequest,
+  onTurn,
+  waitForElement
+} from '@utils';
 
 import addCraftingButtons from '../../shared/crafting-buttons';
 import keepInventoryToggled from '../../shared/folklore-forest/keep-inventory-open';
 
 import regionStyles from '../../shared/folklore-forest/styles.css';
 import styles from './styles.css';
+
+const addQuickReinforce = () => {
+  const wrapper = document.querySelector('.draconicDepthsCavernView__reinforceCavernContainer');
+  if (! wrapper) {
+    return;
+  }
+
+  const reinforceButton = wrapper.querySelector('.draconicDepthsCavernView__reinforceCavernButton');
+  if (! reinforceButton) {
+    return;
+  }
+
+  if (reinforceButton.classList.contains('draconicDepthsCavernView__reinforceCavernButton--quick-reinforce')) {
+    return;
+  }
+
+  const quest = user?.quests?.QuestDraconicDepths;
+  if (! quest) {
+    return;
+  }
+
+  const button = makeElement('button', ['mh-improved-quick-reinforce', 'draconicDepthsCavernView__reinforceCavernButton'], 'Max');
+
+  const canReinforce = quest?.cavern?.reinforcement?.can_reinforce;
+  if (canReinforce) {
+    button.classList.remove('draconicDepthsCavernView__reinforceCavernButton--disabled');
+    button.removeAttribute('disabled');
+  } else {
+    button.classList.add('draconicDepthsCavernView__reinforceCavernButton--disabled');
+    button.setAttribute('disabled', 'disabled');
+  }
+
+  const remaining = quest?.cavern?.hunts_remaining;
+  const max = quest?.cavern?.max_hunts_remaining;
+  const costPerReinforce = quest?.cavern?.reinforcement?.cost[0]?.quantity;
+  const totalCost = (max - remaining) * costPerReinforce;
+  const embers = quest?.items?.dragon_ember?.quantity_unformatted;
+  const canAffordReinforces = totalCost > embers ? Math.floor(embers / costPerReinforce) : max - remaining;
+  if (canAffordReinforces <= 0) {
+    button.classList.add('draconicDepthsCavernView__reinforceCavernButton--disabled');
+    button.setAttribute('disabled', 'disabled');
+  } else {
+    button.classList.remove('draconicDepthsCavernView__reinforceCavernButton--disabled');
+    button.removeAttribute('disabled');
+  }
+
+  button.innerText = canAffordReinforces > 0 ? `Max (+${canAffordReinforces})` : 'Max';
+
+  button.addEventListener('click', async () => {
+    if (button.classList.contains('draconicDepthsCavernView__reinforceCavernButton--disabled')) {
+      return;
+    }
+
+    reinforceButton.click();
+
+    const reinforceInput = await waitForElement('.draconicDepthsReinforceCavernDialogView__reinforceInputBox');
+    if (! reinforceInput) {
+      if (window.activejsdialog) {
+        window.activejsdialog.hide();
+      }
+      return;
+    }
+
+    reinforceInput.value = Math.min(canAffordReinforces, 1);
+    const event = new Event('input', {
+      bubbles: true,
+      cancelable: true,
+    });
+    reinforceInput.dispatchEvent(event);
+
+    const reinforceDialogButton = document.querySelector('.draconicDepthsReinforceCavernDialogView__reinforceButton');
+    if (! reinforceDialogButton) {
+      if (window.activejsdialog) {
+        window.activejsdialog.hide();
+      }
+      return;
+    }
+    setTimeout(() => {
+      reinforceDialogButton.click();
+    }, 300);
+
+    button.innerText = `Max (+${canAffordReinforces})`;
+  });
+
+  reinforceButton.classList.add('draconicDepthsCavernView__reinforceCavernButton--quick-reinforce');
+  wrapper.append(button);
+};
 
 /**
  * Initialize the module.
@@ -41,7 +134,7 @@ export default async () => {
         shop: 'poisonous_provolone_pack_small_convertible',
       },
       elemental_emmental_cheese: {
-        amounts: [10, 50],
+        amounts: [10, 24, 50],
         shopNormal: 'elemental_emmental_cheese',
         shop: 'elemental_emmental_pack_small_convertible',
       }
@@ -53,4 +146,8 @@ export default async () => {
       baitCraftQty: '.headsUpDisplayDraconicDepthsView__ingredientQuantity',
     },
   });
+
+  addQuickReinforce();
+  onTurn(addQuickReinforce, 1000);
+  onRequest('environment/draconic_depths.php', addQuickReinforce);
 };
