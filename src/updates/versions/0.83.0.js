3f1d234b1ced3e1cef019d98113e9290553b3837
Brad Parbs <brad@bradparbs.com>
2025-06-28 23:52:55 -0500

    Remove old draggable data, migrate catch stats

--- src/updates/versions/0.83.0.js
+++ src/updates/versions/0.83.0.js
@@ -1,4 +1,4 @@
-import { clearCaches, saveSetting } from '@utils';
+import { clearCaches, dataSet, saveSetting } from '@utils';
 
 export default {
   version: '0.83.0',
@@ -6,6 +6,17 @@ export default {
     if (getSetting('experiments.full-mice-images-in-journal', false)) {
       saveSetting('better-journal.full-mice-images', true);
       saveSetting('experiments.full-mice-images-in-journal', false);
+    }
+
+    const draggableKeys = Object.keys(localStorage).filter((key) => key.startsWith('mh-draggable-'));
+    draggableKeys.forEach(async (key) => {
+      localStorage.removeItem(key);
+    });
+
+    const catchStatsPosition = localStorage.getItem('mh-catch-stats-position');
+    if (catchStatsPosition) {
+      await dataSet('mh-draggable--mh-catch-stats', catchStatsPosition);
+      localStorage.removeItem('mh-catch-stats-position');
     }
 
     await clearCaches();
