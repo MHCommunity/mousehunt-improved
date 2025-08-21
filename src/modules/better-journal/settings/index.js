3e53f79cb6d942b3689d31ee0a814eacda3f3bef
Brad Parbs <brad@bradparbs.com>
2025-08-20 19:37:43 -0500

    Update better journal settings for improved clarity and consistency

--- src/modules/better-journal/settings/index.js
+++ src/modules/better-journal/settings/index.js
@@ -7,50 +7,50 @@ export default async () => {
   return [
     {
       id: 'better-journal.styles',
-      title: 'Journal styles',
-      default: true,
+      title: 'Style and UI changes',
+      default: true
     },
     {
       id: 'better-journal.replacements',
-      title: 'Journal text replacements',
-      default: true,
-    },
-    {
-      id: 'better-journal.icons',
-      title: 'Show loot icons',
-    },
-    {
-      id: 'better-journal.icons-minimal',
-      title: 'Show loot icons (minimal)',
-    },
-    {
-      id: 'better-journal.list',
-      title: 'Show loot as a list',
+      title: 'Text replacements',
+      default: true
     },
     {
       id: 'better-journal.gold-and-points',
-      title: 'Show gold and points icons',
-      default: true,
+      title: 'Gold and Points icons',
+      default: true
+    },
+    {
+      id: 'better-journal.list',
+      title: 'Loot as list',
+    },
+    {
+      id: 'better-journal.icons',
+      title: 'Loot icons',
+    },
+    {
+      id: 'better-journal.icons-minimal',
+      title: 'Loot icons (minimal)',
     },
     {
       id: 'better-journal.item-colors',
       title: 'Unique item colors (Map clues, Ful\'Mina\'s gifts, etc.)',
-      default: true,
-    },
-    {
-      id: 'better-journal.journal-tags',
-      title: 'Show hunt type on hover',
-      default: false
+      default: true
     },
     {
       id: 'better-journal.journal-history',
       title: 'Journal History',
-      default: true,
+      default: true
     },
     {
       id: 'better-journal.full-mice-images',
-      title: 'Show full mouse images',
-      default: false,
-    }
+      title: 'Full mice images',
+      default: false
+    },
+    {
+      id: 'better-journal.highlight-rare-mice',
+      title: 'Highlight rare mice catches (currently only Black Widow)',
+      default: false
+    },
   ];
 };
