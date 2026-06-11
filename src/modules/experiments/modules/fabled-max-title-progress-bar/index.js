ce4156565da9eaa3f35da5d77d14c55db524fc66
Brad Parbs <brad@bradparbs.com>
2026-06-10 21:09:34 -0500

    add fabled max title progress bar experiment and related styles

---
+++ src/modules/experiments/modules/fabled-max-title-progress-bar/index.js
@@ -0,0 +1,18 @@
+import { addStyles, isUserTitleAtLeast } from '@utils';
+
+import styles from './styles.css';
+
+/**
+ * Initialize the module.
+ */
+const init = async () => {
+  addStyles(styles, 'fabled-max-title-progress-bar');
+};
+
+export default {
+  id: 'experiments.fabled-max-title-progress-bar',
+  name: 'HUD: Show progress bar at max title',
+  description: 'Show the completed title progress bar instead of the max title text.',
+  showIf: () => isUserTitleAtLeast('fabled'),
+  load: init,
+};
